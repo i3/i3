45c3341e09da6558182ad7b8db2812756f0d85e5
Michael Stapelberg <michael@stapelberg.de>
2009-07-28 20:58:56 +0200

    Add docs to include/ipc.h

--- include/ipc.h
+++ include/ipc.h
@@ -16,8 +16,20 @@
 
 #include "i3/ipc.h"
 
+/**
+ * Handler for activity on the listening socket, meaning that a new client
+ * has just connected and we should accept() him. Sets up the event handler
+ * for activity on the new connection and inserts the file descriptor into
+ * the list of clients.
+ *
+ */
 void ipc_new_client(EV_P_ struct ev_io *w, int revents);
 
+/**
+ * Creates the UNIX domain socket at the given path, sets it to non-blocking
+ * mode, bind()s and listen()s on it.
+ *
+ */
 int ipc_create_socket(const char *filename);
 
 #endif
