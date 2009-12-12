i3/i3

commit 5a1668db36d31ee7984c90c33ef5868b4f0d0c29

Bugfix: Render containers after setting the client active (Thanks Mirko)
This lead to race conditions when the window did not change its title
after mapping and was displayed in a tabbed container.

src/manage.c
@@ -444,8 +444,10 @@ map:
         if ((CUR_CELL->workspace->fullscreen_client == NULL || new->fullscreen) && !new->dock) {
                 /* Focus the new window if we’re not in fullscreen mode and if it is not a dock window */
                 if ((new->workspace->fullscreen_client == NULL) || new->fullscreen) {
-                        if (!client_is_floating(new))
+                        if (!client_is_floating(new)) {
                                 new->container->currently_focused = new;
+                                render_container(conn, new->container);
+                        }
                         if (new->container == CUR_CELL || client_is_floating(new))
                                 xcb_set_input_focus(conn, XCB_INPUT_FOCUS_POINTER_ROOT, new->child, XCB_CURRENT_TIME);
                 }

commit ffe925f733d2840bba0083b433aa9dd7586dcd99

Bugfix: Fix assignments to not yet visible workspaces
This was broken by commit 5a1668db3

src/manage.c
@@ -446,7 +446,8 @@ map:
                 if ((new->workspace->fullscreen_client == NULL) || new->fullscreen) {
                         if (!client_is_floating(new)) {
                                 new->container->currently_focused = new;
-                                render_container(conn, new->container);
+                                if (map_frame)
+                                        render_container(conn, new->container);
                         }
                         if (new->container == CUR_CELL || client_is_floating(new))
                                 xcb_set_input_focus(conn, XCB_INPUT_FOCUS_POINTER_ROOT, new->child, XCB_CURRENT_TIME);