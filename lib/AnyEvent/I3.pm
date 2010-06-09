da94674f8fdd98ae324b79b90a1698721a1c6bb2
Michael Stapelberg <michael@stapelberg.de>
2010-06-10 00:20:11 +0200

    bump version

--- lib/AnyEvent/I3.pm
+++ lib/AnyEvent/I3.pm
@@ -15,11 +15,11 @@ AnyEvent::I3 - communicate with the i3 window manager
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =head1 SYNOPSIS
 
