a3a42f30e100ecc84165381a71e140bf32284b5a
Michael Stapelberg <michael@stapelberg.de>
2010-06-16 19:40:55 +0200

    Add check to Makefile to abort in a Windows environment (neither i3 nor unix sockets available)
    Hopefully, this will stop CPAN Testers emails about failing tests on windows.

--- lib/AnyEvent/I3.pm
+++ lib/AnyEvent/I3.pm
@@ -15,11 +15,11 @@ AnyEvent::I3 - communicate with the i3 window manager
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =head1 SYNOPSIS
 
