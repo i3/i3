d9dd245bcc420f2e0d151fe5f1ee659463222221
Michael Stapelberg <michael@stapelberg.de>
2011-01-22 17:00:27 +0100

    Bugfix: Use strdup() for the initial value of socket_path (Thanks mseed)

--- i3-msg/main.c
+++ i3-msg/main.c
@@ -109,7 +109,7 @@ static void ipc_recv_message(int sockfd, uint32_t message_type,
 int main(int argc, char *argv[]) {
         char *socket_path;
         if ((socket_path = getenv("I3SOCK")) == NULL) {
-            socket_path = "/tmp/i3-ipc.sock";
+            socket_path = strdup("/tmp/i3-ipc.sock");
         }
         int o, option_index = 0;
         int message_type = I3_IPC_MESSAGE_TYPE_COMMAND;
