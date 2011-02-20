481ae6ccf22cb87ddc05de05ddfba61a0c48e413
Fernando Tarlá Cardoso Lemos <fernandotcl@gmail.com>
2011-02-20 10:44:58 -0300

    Support pkg-config if the modules are available.

--- common.mk
+++ common.mk
@@ -11,6 +11,11 @@ endif
 GIT_VERSION:="$(shell git describe --tags --always) ($(shell git log --pretty=format:%cd --date=short -n1), branch $(shell [ -f .git/HEAD ] && sed 's/ref: refs\/heads\/\(.*\)/\\\\\\"\1\\\\\\"/g' .git/HEAD || echo 'unknown'))"
 VERSION:=$(shell git describe --tags --abbrev=0)
 
+# An easier way to get CFLAGS and LDFLAGS falling back in case there's
+# no pkg-config support for certain libraries
+cflags_for_lib = $(shell pkg-config --silence-errors --cflags $(1))
+ldflags_for_lib = $(shell pkg-config --exists $(1) && pkg-config --libs $(1) || echo -l$(2))
+
 CFLAGS += -std=c99
 CFLAGS += -pipe
 CFLAGS += -Wall
@@ -19,23 +24,36 @@ CFLAGS += -Wall
 CFLAGS += -Wunused-value
 CFLAGS += -Iinclude
 CFLAGS += -I/usr/local/include
+CFLAGS += $(call cflags_for_lib, xcb-event)
+CFLAGS += $(call cflags_for_lib, xcb-property)
+CFLAGS += $(call cflags_for_lib, xcb-keysyms)
+CFLAGS += $(call cflags_for_lib, xcb-atom)
+CFLAGS += $(call cflags_for_lib, xcb-aux)
+CFLAGS += $(call cflags_for_lib, xcb-icccm)
+CFLAGS += $(call cflags_for_lib, xcb-xinerama)
+CFLAGS += $(call cflags_for_lib, xcb-randr)
+CFLAGS += $(call cflags_for_lib, xcb)
+CFLAGS += $(call cflags_for_lib, xcursor)
+CFLAGS += $(call cflags_for_lib, x11)
+CFLAGS += $(call cflags_for_lib, yajl)
+CFLAGS += $(call cflags_for_lib, libev)
 CFLAGS += -DI3_VERSION=\"${GIT_VERSION}\"
 CFLAGS += -DSYSCONFDIR=\"${SYSCONFDIR}\"
 
 LDFLAGS += -lm
-LDFLAGS += -lxcb-event
-LDFLAGS += -lxcb-property
-LDFLAGS += -lxcb-keysyms
-LDFLAGS += -lxcb-atom
-LDFLAGS += -lxcb-aux
-LDFLAGS += -lxcb-icccm
-LDFLAGS += -lxcb-xinerama
-LDFLAGS += -lxcb-randr
-LDFLAGS += -lxcb
-LDFLAGS += -lyajl
-LDFLAGS += -lXcursor
-LDFLAGS += -lX11
-LDFLAGS += -lev
+LDFLAGS += $(call ldflags_for_lib, xcb-event, xcb-event)
+LDFLAGS += $(call ldflags_for_lib, xcb-property, xcb-property)
+LDFLAGS += $(call ldflags_for_lib, xcb-keysyms, xcb-keysyms)
+LDFLAGS += $(call ldflags_for_lib, xcb-atom, xcb-atom)
+LDFLAGS += $(call ldflags_for_lib, xcb-aux, xcb-aux)
+LDFLAGS += $(call ldflags_for_lib, xcb-icccm, xcb-icccm)
+LDFLAGS += $(call ldflags_for_lib, xcb-xinerama, xcb-xinerama)
+LDFLAGS += $(call ldflags_for_lib, xcb-randr, xcb-randr)
+LDFLAGS += $(call ldflags_for_lib, xcb, xcb)
+LDFLAGS += $(call ldflags_for_lib, xcursor, Xcursor)
+LDFLAGS += $(call ldflags_for_lib, x11, X11)
+LDFLAGS += $(call ldflags_for_lib, yajl, yajl)
+LDFLAGS += $(call ldflags_for_lib, libev, ev)
 LDFLAGS += -L/usr/local/lib -L/usr/pkg/lib
 
 ifeq ($(UNAME),NetBSD)
