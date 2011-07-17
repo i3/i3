fc583adb9956ff95e9019bef8bb0e7c2e21d133b
Michael Stapelberg <michael@stapelberg.de>
2011-07-17 15:17:24 +0200

    makefile: link -liconv on Darwin (Thanks Marcus)

--- common.mk
+++ common.mk
@@ -83,6 +83,10 @@ ifeq ($(UNAME),FreeBSD)
 LIBS += -liconv
 endif
 
+ifeq ($(UNAME),Darwin)
+LIBS += -liconv
+endif
+
 # Fallback for libyajl 1 which did not include yajl_version.h. We need
 # YAJL_MAJOR from that file to decide which code path should be used.
 CFLAGS += -idirafter yajl-fallback
