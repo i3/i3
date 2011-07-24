4fe870c92761ee48210106d3ab83984baacdb1d8
Michael Stapelberg <michael@stapelberg.de>
2011-07-24 22:16:40 +0200

    define prototype for xcb_get_numlock_mask (fixes warning)

--- i3-config-wizard/xcb.h
+++ i3-config-wizard/xcb.h
@@ -11,5 +11,10 @@ uint32_t get_colorpixel(xcb_connection_t *conn, char *hex);
 uint32_t get_mod_mask(xcb_connection_t *conn, uint32_t keycode);
 xcb_window_t open_input_window(xcb_connection_t *conn, uint32_t width, uint32_t height);
 int get_font_id(xcb_connection_t *conn, char *pattern, int *font_height);
+/**
+ * Finds out which modifier mask is the one for numlock, as the user may change this.
+ *
+ */
+void xcb_get_numlock_mask(xcb_connection_t *conn);
 
 #endif
