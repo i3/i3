f84264ae4185a83217a5b412ecc9bbb312a29f96
Michael Stapelberg <michael@stapelberg.de>
2011-07-27 12:51:38 +0200

    remove _NET_WM_WORKAREA from _NET_SUPPORTED for now
    See ticket #301, this is a problem with Qt apps on your second monitor.
    
    We should implement better EWMH support, but not before the release :).

--- src/main.c
+++ src/main.c
@@ -378,7 +378,7 @@ int main(int argc, char *argv[]) {
 #include "atoms.xmacro"
 #undef xmacro
     };
-    xcb_change_property(conn, XCB_PROP_MODE_REPLACE, root, A__NET_SUPPORTED, A_ATOM, 32, 16, supported_atoms);
+    xcb_change_property(conn, XCB_PROP_MODE_REPLACE, root, A__NET_SUPPORTED, A_ATOM, 32, 15, supported_atoms);
     /* Set up the window manager’s name */
     xcb_change_property(conn, XCB_PROP_MODE_REPLACE, root, A__NET_SUPPORTING_WM_CHECK, A_WINDOW, 32, 1, &root);
     xcb_change_property(conn, XCB_PROP_MODE_REPLACE, root, A__NET_WM_NAME, A_UTF8_STRING, 8, strlen("i3"), "i3");
