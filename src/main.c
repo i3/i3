bc7dec6a4cc5b0338ede0e7f215772d79b7f7ddf
Michael Stapelberg <michael@stapelberg.de>
2011-07-29 13:11:02 +0200

    X11 errors: also log the error_code (see X Protocol, Appendix B, Errors)

--- src/main.c
+++ src/main.c
@@ -76,7 +76,11 @@ static void xcb_check_cb(EV_P_ ev_check *w, int revents) {
         if (event->response_type == 0) {
             if (event_is_ignored(event->sequence, 0))
                 DLOG("Expected X11 Error received for sequence %x\n", event->sequence);
-            else ELOG("X11 Error received! sequence %x\n", event->sequence);
+            else {
+                xcb_generic_error_t *error = (xcb_generic_error_t*)event;
+                ELOG("X11 Error received! sequence 0x%x, error_code = %d\n",
+                     error->sequence, error->error_code);
+            }
             continue;
         }
 
