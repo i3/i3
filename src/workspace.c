8928b5f55de9ad2e13b3f50f9bec5d42221eeb30
Michael Stapelberg <michael@stapelberg.de>
2011-09-05 22:21:10 +0200

    Bugfix: Correctly handle workspace names which do not start with a zero

--- src/workspace.c
+++ src/workspace.c
@@ -49,11 +49,12 @@ Con *workspace_get(const char *num, bool *created) {
         workspace->name = sstrdup(num);
         /* We set ->num to the number if this workspace’s name consists only of
          * a positive number. Otherwise it’s a named ws and num will be -1. */
-
-        long parsed_num = strtol(num, NULL, 10);
+        char *endptr = NULL;
+        long parsed_num = strtol(num, &endptr, 10);
         if (parsed_num == LONG_MIN ||
             parsed_num == LONG_MAX ||
-            parsed_num < 0)
+            parsed_num < 0 ||
+            endptr == num)
             workspace->num = -1;
         else workspace->num = parsed_num;
         LOG("num = %d\n", workspace->num);
