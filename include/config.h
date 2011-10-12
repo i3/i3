f26a344dfa00d3b53a92e374612833e31ff11705
Michael Stapelberg <michael@stapelberg.de>
2011-10-12 22:29:39 +0100

    reformat include/config.h

--- include/config.h
+++ include/config.h
@@ -1,5 +1,5 @@
 /*
- * vim:ts=8:expandtab
+ * vim:ts=4:sw=4:expandtab
  *
  * i3 - an improved dynamic tiling window manager
  *
@@ -32,17 +32,17 @@ extern SLIST_HEAD(modes_head, Mode) modes;
  *
  */
 struct context {
-        bool has_errors;
+    bool has_errors;
 
-        int line_number;
-        char *line_copy;
-        const char *filename;
+    int line_number;
+    char *line_copy;
+    const char *filename;
 
-        char *compact_error;
+    char *compact_error;
 
-        /* These are the same as in YYLTYPE */
-        int first_column;
-        int last_column;
+    /* These are the same as in YYLTYPE */
+    int first_column;
+    int last_column;
 };
 
 /**
@@ -51,9 +51,9 @@ struct context {
  *
  */
 struct Colortriple {
-        uint32_t border;
-        uint32_t background;
-        uint32_t text;
+    uint32_t border;
+    uint32_t background;
+    uint32_t text;
 };
 
 /**
@@ -62,11 +62,11 @@ struct Colortriple {
  *
  */
 struct Variable {
-        char *key;
-        char *value;
-        char *next_match;
+    char *key;
+    char *value;
+    char *next_match;
 
-        SLIST_ENTRY(Variable) variables;
+    SLIST_ENTRY(Variable) variables;
 };
 
 /**
@@ -76,10 +76,10 @@ struct Variable {
  *
  */
 struct Mode {
-        char *name;
-        struct bindings_head *bindings;
+    char *name;
+    struct bindings_head *bindings;
 
-        SLIST_ENTRY(Mode) modes;
+    SLIST_ENTRY(Mode) modes;
 };
 
 /**
@@ -88,86 +88,86 @@ struct Mode {
  *
  */
 struct Config {
-        const char *terminal;
-        i3Font font;
+    const char *terminal;
+    i3Font font;
 
-        char *ipc_socket_path;
-        const char *restart_state_path;
+    char *ipc_socket_path;
+    const char *restart_state_path;
 
-        int default_layout;
-        int container_stack_limit;
-        int container_stack_limit_value;
+    int default_layout;
+    int container_stack_limit;
+    int container_stack_limit_value;
 
-        /** Default orientation for new containers */
-        int default_orientation;
+    /** Default orientation for new containers */
+    int default_orientation;
 
-        /** By default, focus follows mouse. If the user explicitly wants to
-         * turn this off (and instead rely only on the keyboard for changing
-         * focus), we allow him to do this with this relatively special option.
-         * It is not planned to add any different focus models. */
-        bool disable_focus_follows_mouse;
+    /** By default, focus follows mouse. If the user explicitly wants to
+     * turn this off (and instead rely only on the keyboard for changing
+     * focus), we allow him to do this with this relatively special option.
+     * It is not planned to add any different focus models. */
+    bool disable_focus_follows_mouse;
 
-        /** By default, a workspace bar is drawn at the bottom of the screen.
-         * If you want to have a more fancy bar, it is recommended to replace
-         * the whole bar by dzen2, for example using the i3-wsbar script which
-         * comes with i3. Thus, you can turn it off entirely. */
-        bool disable_workspace_bar;
+    /** By default, a workspace bar is drawn at the bottom of the screen.
+     * If you want to have a more fancy bar, it is recommended to replace
+     * the whole bar by dzen2, for example using the i3-wsbar script which
+     * comes with i3. Thus, you can turn it off entirely. */
+    bool disable_workspace_bar;
 
-        /** Think of the following layout: Horizontal workspace with a tabbed
-         * con on the left of the screen and a terminal on the right of the
-         * screen. You are in the second container in the tabbed container and
-         * focus to the right. By default, i3 will set focus to the terminal on
-         * the right. If you are in the first container in the tabbed container
-         * however, focusing to the left will wrap. This option forces i3 to
-         * always wrap, which will result in you having to use "focus parent"
-         * more often. */
-        bool force_focus_wrapping;
+    /** Think of the following layout: Horizontal workspace with a tabbed
+     * con on the left of the screen and a terminal on the right of the
+     * screen. You are in the second container in the tabbed container and
+     * focus to the right. By default, i3 will set focus to the terminal on
+     * the right. If you are in the first container in the tabbed container
+     * however, focusing to the left will wrap. This option forces i3 to
+     * always wrap, which will result in you having to use "focus parent"
+     * more often. */
+    bool force_focus_wrapping;
 
-        /** By default, use the RandR API for multi-monitor setups.
-         * Unfortunately, the nVidia binary graphics driver doesn't support
-         * this API. Instead, it only support the less powerful Xinerama API,
-         * which can be enabled by this option.
-         *
-         * Note: this option takes only effect on the initial startup (eg.
-         * reconfiguration is not possible). On startup, the list of screens
-         * is fetched once and never updated. */
-        bool force_xinerama;
+    /** By default, use the RandR API for multi-monitor setups.
+     * Unfortunately, the nVidia binary graphics driver doesn't support
+     * this API. Instead, it only support the less powerful Xinerama API,
+     * which can be enabled by this option.
+     *
+     * Note: this option takes only effect on the initial startup (eg.
+     * reconfiguration is not possible). On startup, the list of screens
+     * is fetched once and never updated. */
+    bool force_xinerama;
 
-        /** Automatic workspace back and forth switching. If this is set, a
-         * switch to the currently active workspace will switch to the
-         * previously focused one instead, making it possible to fast toggle
-         * between two workspaces. */
-        bool workspace_auto_back_and_forth;
+    /** Automatic workspace back and forth switching. If this is set, a
+     * switch to the currently active workspace will switch to the
+     * previously focused one instead, making it possible to fast toggle
+     * between two workspaces. */
+    bool workspace_auto_back_and_forth;
 
-        /** The default border style for new windows. */
-        border_style_t default_border;
+    /** The default border style for new windows. */
+    border_style_t default_border;
 
-        /** The default border style for new floating windows. */
-        border_style_t default_floating_border;
+    /** The default border style for new floating windows. */
+    border_style_t default_floating_border;
 
-        /** The modifier which needs to be pressed in combination with your mouse
-         * buttons to do things with floating windows (move, resize) */
-        uint32_t floating_modifier;
+    /** The modifier which needs to be pressed in combination with your mouse
+     * buttons to do things with floating windows (move, resize) */
+    uint32_t floating_modifier;
 
-        /* Color codes are stored here */
-        struct config_client {
-                uint32_t background;
-                struct Colortriple focused;
-                struct Colortriple focused_inactive;
-                struct Colortriple unfocused;
-                struct Colortriple urgent;
-        } client;
-        struct config_bar {
-                struct Colortriple focused;
-                struct Colortriple unfocused;
-                struct Colortriple urgent;
-        } bar;
+    /* Color codes are stored here */
+    struct config_client {
+        uint32_t background;
+        struct Colortriple focused;
+        struct Colortriple focused_inactive;
+        struct Colortriple unfocused;
+        struct Colortriple urgent;
+    } client;
+    struct config_bar {
+        struct Colortriple focused;
+        struct Colortriple unfocused;
+        struct Colortriple urgent;
+    } bar;
 
-        /** What should happen when a new popup is opened during fullscreen mode */
-        enum {
-                PDF_LEAVE_FULLSCREEN = 0,
-                PDF_IGNORE = 1
-        } popup_during_fullscreen;
+    /** What should happen when a new popup is opened during fullscreen mode */
+    enum {
+        PDF_LEAVE_FULLSCREEN = 0,
+        PDF_IGNORE = 1
+    } popup_during_fullscreen;
 };
 
 /**
