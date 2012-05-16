ec4e6d1cdf5b76c79da2879635e4094e25a01f3d
Michael Stapelberg <michael@stapelberg.de>
2012-05-16 06:24:16 +0200

    Fix stray workspaces "number 1" being created when having workspace number 1 in your config

--- src/workspace.c
+++ src/workspace.c
@@ -112,14 +112,15 @@ Con *create_workspace_on_output(Output *output, Con *content) {
         DLOG("relevant command = %s\n", bind->command);
         char *target = bind->command + strlen("workspace ");
         /* We check if this is the workspace
-         * next/prev/next_on_output/prev_on_output/back_and_forth command.
+         * next/prev/next_on_output/prev_on_output/back_and_forth/number command.
          * Beware: The workspace names "next", "prev", "next_on_output",
-         * "prev_on_output" and "back_and_forth" are OK, so we check before
-         * stripping the double quotes */
+         * "prev_on_output", "number" and "back_and_forth" are OK, so we check
+         * before stripping the double quotes */
         if (strncasecmp(target, "next", strlen("next")) == 0 ||
             strncasecmp(target, "prev", strlen("prev")) == 0 ||
             strncasecmp(target, "next_on_output", strlen("next_on_output")) == 0 ||
             strncasecmp(target, "prev_on_output", strlen("prev_on_output")) == 0 ||
+            strncasecmp(target, "number", strlen("number")) == 0 ||
             strncasecmp(target, "back_and_forth", strlen("back_and_forth")) == 0)
             continue;
         if (*target == '"')
