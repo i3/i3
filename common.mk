f06161b58a20dc2c3cdeca849dd3cd4dca20e812
Quentin Glidic <sardemff7+git@sardemff7.net>
2012-08-05 20:54:30 +0200

    common.mk: Check for Pango

--- common.mk
+++ common.mk
@@ -135,6 +135,13 @@ PCRE_LIBS   := $(call ldflags_for_lib, libpcre,pcre)
 LIBSN_CFLAGS := $(call cflags_for_lib, libstartup-notification-1.0)
 LIBSN_LIBS   := $(call ldflags_for_lib, libstartup-notification-1.0,startup-notification-1)
 
+# Pango
+PANGO_CFLAGS := $(call cflags_for_lib, cairo)
+PANGO_CFLAGS += $(call cflags_for_lib, pangocairo)
+I3_CPPFLAGS  += -DPANGO_SUPPORT=1
+PANGO_LIBS   := $(call ldflags_for_lib, cairo)
+PANGO_LIBS   += $(call ldflags_for_lib, pangocairo)
+
 # libi3
 LIBS = -L$(TOPDIR) -li3
 
