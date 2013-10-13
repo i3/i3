i3/i3

commit 97b086efd9833d2a787e54417789b279143e43a6

Close all children when closing a workspace
fixes #591

src/tree.c
@@ -229,6 +229,11 @@ bool tree_close(Con *con, kill_window_t kill_window, bool dont_kill_parent, bool
         return false;
     }
 
+    if (workspace_is_visible(con)) {
+        DLOG("A visible workspace cannot be killed.\n");
+        return false;
+    }
+
     if (con->window != NULL) {
         if (kill_window != DONT_KILL_WINDOW) {
             x_window_kill(con->window->id, kill_window);
@@ -359,10 +364,6 @@ bool tree_close(Con *con, kill_window_t kill_window, bool dont_kill_parent, bool
  */
 void tree_close_con(kill_window_t kill_window) {
     assert(focused != NULL);
-    if (focused->type == CT_WORKSPACE) {
-        LOG("Cannot close workspace\n");
-        return;
-    }
 
     /* There *should* be no possibility to focus outputs / root container */
     assert(focused->type != CT_OUTPUT);

testcases/t/129-focus-after-close.t
@@ -120,6 +120,30 @@ sync_with_i3;
 is(get_focused($tmp), $middle, 'middle container focused');
 
 ##############################################################
+# check if the workspace container can be closed
+##############################################################
+
+$tmp = fresh_workspace;
+
+my $window = open_window();
+
+# one window opened on the current workspace
+($nodes, $focus) = get_ws_content($tmp);
+is(scalar @$nodes, 1, 'workspace contains one node');
+
+# focus the workspace
+cmd "focus parent";
+cmd "focus parent";
+
+# try to kill the workspace
+cmd "kill";
+sync_with_i3;
+
+# the workspace should now be empty
+($nodes, $focus) = get_ws_content($tmp);
+is(scalar @$nodes, 0, 'workspace is empty');
+
+##############################################################
 # and now for something completely different:
 # check if the pointer position is relevant when restoring focus
 # (it should not be relevant, of course)

commit eef9042713bd9b26c23e42f1bf07100113e1f599

Fix endless loop when trying to kill a visible workspace
This regression was introduced with commit
97b086efd9833d2a787e54417789b279143e43a6

fixes #1103

src/tree.c
@@ -229,11 +229,6 @@ bool tree_close(Con *con, kill_window_t kill_window, bool dont_kill_parent, bool
         return false;
     }
 
-    if (workspace_is_visible(con)) {
-        DLOG("A visible workspace cannot be killed.\n");
-        return false;
-    }
-
     if (con->window != NULL) {
         if (kill_window != DONT_KILL_WINDOW) {
             x_window_kill(con->window->id, kill_window);
@@ -368,6 +363,19 @@ void tree_close_con(kill_window_t kill_window) {
     /* There *should* be no possibility to focus outputs / root container */
     assert(focused->type != CT_OUTPUT);
     assert(focused->type != CT_ROOT);
+
+    if (focused->type == CT_WORKSPACE) {
+        DLOG("Workspaces cannot be close, closing all children instead\n");
+        Con *child, *nextchild;
+        for (child = TAILQ_FIRST(&(focused->nodes_head)); child; ) {
+            nextchild = TAILQ_NEXT(child, nodes);
+            DLOG("killing child=%p\n", child);
+            tree_close(child, kill_window, false, false);
+            child = nextchild;
+        }
+
+        return;
+    }
 
     /* Kill con */
     tree_close(focused, kill_window, false, false);