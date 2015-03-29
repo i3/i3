e622c42ef087e50bbf9666b794b9601788029873
Deiz <silverwraithii@gmail.com>
2015-03-29 17:18:00 -0400

    Move mkdirp into libi3

--- src/ipc.c
+++ src/ipc.c
@@ -38,36 +38,6 @@ static void set_nonblock(int sockfd) {
 }
 
 /*
- * Emulates mkdir -p (creates any missing folders)
- *
- */
-bool mkdirp(const char *path) {
-    if (mkdir(path, S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH) == 0)
-        return true;
-    if (errno != ENOENT) {
-        ELOG("mkdir(%s) failed: %s\n", path, strerror(errno));
-        return false;
-    }
-    char *copy = sstrdup(path);
-    /* strip trailing slashes, if any */
-    while (copy[strlen(copy) - 1] == '/')
-        copy[strlen(copy) - 1] = '\0';
-
-    char *sep = strrchr(copy, '/');
-    if (sep == NULL) {
-        FREE(copy);
-        return false;
-    }
-    *sep = '\0';
-    bool result = false;
-    if (mkdirp(copy))
-        result = mkdirp(path);
-    free(copy);
-
-    return result;
-}
-
-/*
  * Sends the specified event to all IPC clients which are currently connected
  * and subscribed to this kind of event.
  *
