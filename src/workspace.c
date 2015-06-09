52c72e900756b89b617ac31dd7fdc0d19c7390d8
Ingo Bürk <ingo.buerk@tngtech.com>
2015-06-09 22:14:56 +0200

    Remove redundant and broken check for '\0'.
    fixes #1742

--- src/workspace.c
+++ src/workspace.c
@@ -129,7 +129,7 @@ Con *create_workspace_on_output(Output *output, Con *content) {
             continue;
         DLOG("relevant command = %s\n", bind->command);
         const char *target = bind->command + strlen("workspace ");
-        while ((*target == ' ' || *target == '\t') && target != '\0')
+        while (*target == ' ' || *target == '\t')
             target++;
         /* We check if this is the workspace
          * next/prev/next_on_output/prev_on_output/back_and_forth/number command.
