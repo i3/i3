1d6827a0fd50d38ff8e518d5e01117257023ced0
Ingo Bürk <ingo.buerk@tngtech.com>
2015-11-02 09:11:21 -0500

    Raise dependency to cairo 1.14.4.
    With the recent cairo bugfix, we can now switch to using cairo for
    rendering i3bar per default by raising the minimum version of cairo
    to 1.14.4, if this cairo version is available.
    
    resolves #2048

--- common.mk
+++ common.mk
@@ -141,6 +141,9 @@ LIBSN_LIBS   := $(call ldflags_for_lib, libstartup-notification-1.0,startup-noti
 PANGO_CFLAGS := $(call cflags_for_lib, cairo)
 PANGO_CFLAGS += $(call cflags_for_lib, pangocairo)
 I3_CPPFLAGS  += -DPANGO_SUPPORT=1
+ifeq ($(shell $(PKG_CONFIG) --atleast-version=1.14.4 cairo 2>/dev/null && echo 1),1)
+I3_CPPFLAGS  += -DI3BAR_CAIRO=1
+endif
 PANGO_LIBS   := $(call ldflags_for_lib, cairo)
 PANGO_LIBS   += $(call ldflags_for_lib, pangocairo)
 
