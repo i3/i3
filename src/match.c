68e3cb8ab79267e8ee0faf0a4e2d60e9dde0e1b8
Ingo Bürk <ingo.buerk@tngtech.com>
2015-12-17 09:43:34 -0500

    Don't force base 10 for parsing id.

--- src/match.c
+++ src/match.c
@@ -297,7 +297,7 @@ void match_parse_property(Match *match, const char *ctype, const char *cvalue) {
 
     if (strcmp(ctype, "id") == 0) {
         char *end;
-        long parsed = strtol(cvalue, &end, 10);
+        long parsed = strtol(cvalue, &end, 0);
         if (parsed == LONG_MIN ||
             parsed == LONG_MAX ||
             parsed < 0 ||
