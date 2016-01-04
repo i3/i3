8bfd06c3dd6e15b031c4a1bca82499e449f5121c
Johannes Lange <johannes.lange@rwth-aachen.de>
2016-01-04 17:31:47 +0100

    added "toggle" option to "split" command
    as requested in #1814

--- include/commands.h
+++ include/commands.h
@@ -157,7 +157,7 @@ void cmd_floating(I3_CMD, const char *floating_mode);
 void cmd_move_workspace_to_output(I3_CMD, const char *name);
 
 /**
- * Implementation of 'split v|h|vertical|horizontal'.
+ * Implementation of 'split v|h|t|vertical|horizontal|toggle'.
  *
  */
 void cmd_split(I3_CMD, const char *direction);
