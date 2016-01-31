6fb52f90cd3e03ea152913033fae666ebfe8f8df
Michael Forster <email@michael-forster.de>
2016-01-31 15:55:44 +0100

    Use con_is_floating instead of checking manually.

--- src/tiling_drag.c
+++ src/tiling_drag.c
@@ -20,7 +20,7 @@ static Con *find_drop_target(uint32_t x, uint32_t y) {
     Con *con;
     TAILQ_FOREACH(con, &all_cons, all_cons) {
         if (con->window != NULL && rect_contains(con->rect, x, y) &&
-            con->floating < FLOATING_AUTO_ON &&
+            !con_is_floating(con) &&
             workspace_is_visible(con_get_workspace(con)) &&
             !con_is_hidden(con))
             return con;
