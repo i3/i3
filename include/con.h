af189f0edea96165ccb893894820bf32c2b0b4db
Michael Forster <email@michael-forster.de>
2016-01-31 20:50:46 +0100

    Fix a typo.

--- include/con.h
+++ include/con.h
@@ -246,9 +246,9 @@ void con_enable_fullscreen(Con *con, fullscreen_mode_t fullscreen_mode);
 void con_disable_fullscreen(Con *con);
 
 /**
- * Moves a container to the side of a target container. This makes the the
- * container and its target the only two children of a split container with
- * appropriate layout.
+ * Moves a container to the side of a target container. This makes the container
+ * and its target the only two children of a split container with appropriate
+ * layout.
  *
  */
 void con_move_to_side_of_con(Con *con, Con *target, direction_t direction);
