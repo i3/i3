cb4235706fd4cc076b7f9eb8b009238b7a1b21d2
Michael Stapelberg <michael@stapelberg.de>
2016-03-06 16:27:49 +0100

    update release.sh for the 4.12 release

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 # This script is used to prepare a new release of i3.
 
-export RELEASE_VERSION="4.11"
-export PREVIOUS_VERSION="4.10.4"
+export RELEASE_VERSION="4.12"
+export PREVIOUS_VERSION="4.11"
 export RELEASE_BRANCH="next"
 
 if [ ! -e "../i3.github.io" ]
