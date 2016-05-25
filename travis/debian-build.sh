ef0a5b0534793c4027b93fb429192ed7db7d0f57
Michael Stapelberg <michael@stapelberg.de>
2016-05-25 08:23:45 +0200

    travis: remove libxcb-xrm-dev from build-deps before dpkg-buildpackage

--- travis/debian-build.sh
+++ travis/debian-build.sh
@@ -11,6 +11,7 @@ make dist
 mkdir -p "${DEST}"
 tar xf *.tar.bz2 -C "${DEST}" --strip-components=1
 cp -r debian "${DEST}"
+sed -i '/^\s*libxcb-xrm-dev/d' deb/ubuntu-*/DIST/debian/control || true
 cd "${DEST}"
 debchange -m -l+g$(git describe --tags) 'Automatically built'
 dpkg-buildpackage -b
