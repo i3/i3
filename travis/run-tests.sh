6584f70d28a91bd9cd2108b1caad3ae5b1b9fa16
Michael Stapelberg <michael@stapelberg.de>
2016-09-05 08:42:34 +0200

    travis: work around asan broken-ness with libc ≥ 2.24
    fixes #2437

--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -4,6 +4,23 @@ set -e
 set -x
 
 cd testcases
+
+# TODO: remove this workaround once https://bugs.debian.org/836723 is fixed
+# Found at https://llvm.org/bugs/show_bug.cgi?id=27310#c8:
+cat >fixasan.c <<EOT
+void __isoc99_printf() {}
+void __isoc99_sprintf() {}
+void __isoc99_snprintf() {}
+void __isoc99_fprintf() {}
+void __isoc99_vprintf() {}
+void __isoc99_vsprintf() {}
+void __isoc99_vsnprintf() {}
+void __isoc99_vfprintf() {}
+void __cxa_throw() {} // NEW
+EOT
+gcc fixasan.c -o fixasan.so -fPIC -shared -nostdlib
+export LD_PRELOAD=$PWD/fixasan.so
+
 # Try running the tests in parallel so that the common case (tests pass) is
 # quick, but fall back to running them in sequence to make debugging easier.
 if ! xvfb-run ./complete-run.pl
