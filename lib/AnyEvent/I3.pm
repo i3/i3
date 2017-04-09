db35244ee766869a1b1b832cddcb311d1f8ab11c
Michael Stapelberg <michael@stapelberg.de>
2017-04-09 15:34:36 +0200

    tag 0.17

--- lib/AnyEvent/I3.pm
+++ lib/AnyEvent/I3.pm
@@ -16,11 +16,11 @@ AnyEvent::I3 - communicate with the i3 window manager
 
 =cut
 
-our $VERSION = '0.16';
+our $VERSION = '0.17';
 
 =head1 VERSION
 
-Version 0.16
+Version 0.17
 
 =head1 SYNOPSIS
 
