1b0205fc5b3c72c7be895894cf483955ce6d139b
Muhmud Ahmad <muhmud.ahmad@googlemail.com>
2017-04-30 00:22:41 +0800

    Tidy up of some debugging messages, etc.

--- src/handlers.c
+++ src/handlers.c
@@ -1365,8 +1365,6 @@ static void property_notify(uint8_t state, xcb_window_t window, xcb_atom_t atom)
  *
  */
 void handle_event(int type, xcb_generic_event_t *event) {
-    DLOG("handle_event()\n");
-    
     if (type != XCB_MOTION_NOTIFY)
         DLOG("event type %d, xkb_base %d\n", type, xkb_base);
     
@@ -1405,9 +1403,9 @@ void handle_event(int type, xcb_generic_event_t *event) {
         } else if (state->xkbType == XCB_XKB_STATE_NOTIFY) {
             DLOG("xkb state group = %d\n", state->group);
 
-            /* If the Alt or Windows Key is released generate an ipc event */
+            /* If the Alt, Windows or any other mod key is released generate an ipc event */
             if (state->eventType == XCB_KEY_RELEASE) {
-                DLOG("State notify release keycode: %d\n", state->keycode);
+                DLOG("Release keycode: %d\n", state->keycode);
                 char *message;
                 sasprintf(&message, "{ \"change\": \"%d\" }", state->keycode);
                 ipc_send_event("key_release", I3_IPC_EVENT_KEY_RELEASE, message);
@@ -1423,8 +1421,7 @@ void handle_event(int type, xcb_generic_event_t *event) {
         
         return;
     }
-
-    DLOG("type %d (%d)\n", type, XCB_KEY_RELEASE);
+    
     switch (type) {
         case XCB_KEY_PRESS:
         case XCB_KEY_RELEASE:
