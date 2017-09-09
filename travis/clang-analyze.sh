1a2ce3edaf4cb81acf1cda504151c8d96ff27a60
Michael Stapelberg <stapelberg@users.noreply.github.com>
2017-09-09 18:30:17 +0200

    travis: remove clang-analyze (#2917)
    • The output currently contains a large number of false-positives and — AFAICT —
      no actual issues.
    
    • Upstream shows little interest in addressing the long-standing issues with the
      TAILQ macros, so the false-positive situation probably won’t change soon:
      https://bugs.llvm.org/show_bug.cgi?id=18222
    
    Currently, we largely spend travis CPU cycles on this, for no additional value.

--- travis/clang-analyze.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-set -x
-
-mkdir -p deb/DIST-clang/build
-tar xf build/*.tar.bz2 -C deb/DIST-clang --strip-components=1
-(cd deb/DIST-clang/build && scan-build -o ../../CLANG ../configure && scan-build -o ../../CLANG --html-title="Analysis of i3 v$(git describe --tags)" make -j8)
-mv deb/CLANG/*/* deb/CLANG
