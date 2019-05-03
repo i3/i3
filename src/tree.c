8c25bc1bd453535a21fb02e4cf5236fe4e3a7062
Orestis Floros <orestisf1993@gmail.com>
2019-05-03 15:39:04 +0300

    tree_close_internal: Log workspace name in EWMH message

--- src/tree.c
+++ src/tree.c
@@ -300,7 +300,7 @@ bool tree_close_internal(Con *con, kill_window_t kill_window, bool dont_kill_par
     x_con_kill(con);
 
     if (ws == con) {
-        DLOG("Closing a workspace container, updating EWMH atoms\n");
+        DLOG("Closing workspace container %s, updating EWMH atoms\n", ws->name);
         ewmh_update_number_of_desktops();
         ewmh_update_desktop_names();
         ewmh_update_wm_desktop();
