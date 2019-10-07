i3/i3

commit 626af81232e6ca81abea22267f3330c16d804596

Fix crash when moving container to marked workspace
Was a small typo.

This also has the (positive) side-effect of allowing to move all the
content of a marked workspace next to the target container, see added
tests.

src/con.c
@@ -1305,7 +1305,7 @@ bool con_move_to_mark(Con *con, const char *mark) {
         return true;
     }
 
-    if (con->type == CT_WORKSPACE) {
+    if (target->type == CT_WORKSPACE) {
         DLOG("target container is a workspace, simply moving the container there.\n");
         con_move_to_workspace(con, target, true, false, false);
         return true;

testcases/t/243-move-to-mark.t
@@ -337,5 +337,49 @@ sync_with_i3;
 does_i3_live;
 
 ###############################################################################
+# Given 'S' and 'M' where 'M' is a workspace and 'S' is on a different
+# workspace, then 'S' ends up as a tiling container on 'M'.
+###############################################################################
+
+fresh_workspace;
+$S = open_window;
+$target_ws = fresh_workspace;
+$M = $target_ws;
+cmd 'mark target';
+
+cmd '[id="' . $S->{id} . '"] move container to mark target';
+sync_with_i3;
+
+does_i3_live;
+
+($nodes, $focus) = get_ws_content($target_ws);
+is(@{$nodes}, 1, 'tiling container moved to the target workspace');
+
+###############################################################################
+# Given 'S' and 'M' where 'S' is a workspace and 'M' is a container on a
+# different workspace, then all the contents of workspace 'S' end up in 'M's
+# workspace.
+###############################################################################
+
+$S = fresh_workspace;
+cmd 'mark S';
+open_window;
+open_window;
+cmd 'splitv';
+open_window;
+open_floating_window;
+$target_ws = fresh_workspace;
+$M = open_window;
+cmd 'mark target';
+
+cmd '[con_mark=S] move container to mark target';
+sync_with_i3;
+
+($nodes, $focus) = get_ws_content($target_ws);
+is(@{$nodes}, 2, 'there is a window and a container with the contents of the original workspace');
+is($nodes->[0]->{window}, $M->{id}, 'M remains the first window');
+is(@{get_ws($target_ws)->{floating_nodes}}, 1, 'target workspace has the floating container');
+
+###############################################################################
 
 done_testing;

commit beb96ad18cfad44e54643af1af0f9900216e8b85

Move container to marked workspace: refine corner case
This commit should fix "move con to parent" trick (see below) in the
case when con->parent->parent is a workspace.

The trick:

    mark _a, focus parent, focus parent, mark _b,
    [con_mark=_a] move window to mark _b, [con_mark=_a] focus

The trick got broken in commit 626af81232e6ca81abea22267f3330c16d804596
in order to fix an i3 crash (#2003).  Reverting said commit fixes the
trick.  The crash is caused by the fact that empty workspace isn't
considered a split (checked in src/con.c:1324), so the moved window ends
up as a sibling of the target workspace, not as its child.

src/con.c
@@ -1312,8 +1312,8 @@ bool con_move_to_mark(Con *con, const char *mark) {
         return true;
     }
 
-    if (target->type == CT_WORKSPACE) {
-        DLOG("target container is a workspace, simply moving the container there.\n");
+    if (target->type == CT_WORKSPACE && con_is_leaf(target)) {
+        DLOG("target container is an empty workspace, simply moving the container there.\n");
         con_move_to_workspace(con, target, true, false, false);
         return true;
     }

testcases/lib/i3test.pm.in
@@ -1230,7 +1230,7 @@ sub create_layout {
 
             $r = $r . '{"swallows": [{';
             $r = $r . '"class": "^' . "$char" . '$"';
-            $r = $r . '}]},';
+            $r = $r . '}]}' . ($depth == 0 ? "\n" : ',');
         } else {
             die "Could not understand $char";
         }

testcases/t/243-move-to-mark.t
@@ -361,6 +361,7 @@ does_i3_live;
 ###############################################################################
 # Given 'S' and 'M' where 'M' is a workspace and 'S' is on a different
 # workspace, then 'S' ends up as a tiling container on 'M'.
+# See issue: #2003
 ###############################################################################
 
 fresh_workspace;

testcases/t/306-move-to-parent.t
@@ -1,0 +1,37 @@
+#!perl
+# vim:ts=4:sw=4:expandtab
+#
+# Please read the following documents before working on tests:
+# • https://build.i3wm.org/docs/testsuite.html
+#   (or docs/testsuite)
+#
+# • https://build.i3wm.org/docs/lib-i3test.html
+#   (alternatively: perldoc ./testcases/lib/i3test.pm)
+#
+# • https://build.i3wm.org/docs/ipc.html
+#   (or docs/ipc)
+#
+# • http://onyxneon.com/books/modern_perl/modern_perl_a4.pdf
+#   (unless you are already familiar with Perl)
+#
+# Make sure the trick used to move the container to its parent works.
+# https://github.com/i3/i3/issues/1326#issuecomment-349082811
+use i3test;
+
+cmp_tree(
+    msg => 'Move to parent when the parent is a workspace',
+    layout_before => 'a H[b*] c',
+    layout_after => 'a b* c',
+    cb => sub {
+        cmd 'mark _a, focus parent, focus parent, mark _b, [con_mark=_a] move window to mark _b, [con_mark=_a] focus';
+    });
+
+cmp_tree(
+    msg => 'Move to parent when the parent is a split',
+    layout_before => 'V[a H[b*] c]',
+    layout_after => 'V[a b* c]',
+    cb => sub {
+        cmd 'mark _a, focus parent, focus parent, mark _b, [con_mark=_a] move window to mark _b, [con_mark=_a] focus';
+    });
+
+done_testing;