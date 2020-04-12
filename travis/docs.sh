f2b176240b3102c0e89cac2164c4fa0b6e68607b
Michael Stapelberg <michael@stapelberg.de>
2020-04-12 11:00:41 +0200

    pod2html: render without stylesheet by default
    fixes #3956

--- travis/docs.sh
+++ travis/docs.sh
@@ -7,10 +7,10 @@ for f in $(grep '\.html$' debian/i3-wm.docs | grep -v 'docs/refcard.html' | grep
 do
 	asciidoc -a linkcss -a stylesdir=https://i3wm.org/css -a scriptsdir=https://i3wm.org/js --backend=xhtml11 -f docs/asciidoc-git.conf $(dirname $f)/$(basename $f .html)
 done
-./docs/i3-pod2html i3-dmenu-desktop man/i3-dmenu-desktop.html
-./docs/i3-pod2html i3-save-tree man/i3-save-tree.html
-./docs/i3-pod2html build/testcases/lib/i3test.pm docs/lib-i3test.html
-./docs/i3-pod2html testcases/lib/i3test/Test.pm docs/lib-i3test-test.html
+./docs/i3-pod2html --stylesurl=https://i3wm.org/css i3-dmenu-desktop man/i3-dmenu-desktop.html
+./docs/i3-pod2html --stylesurl=https://i3wm.org/css i3-save-tree man/i3-save-tree.html
+./docs/i3-pod2html --stylesurl=https://i3wm.org/css build/testcases/lib/i3test.pm docs/lib-i3test.html
+./docs/i3-pod2html --stylesurl=https://i3wm.org/css testcases/lib/i3test/Test.pm docs/lib-i3test-test.html
 for file in $(sed 's/\.1$/.man/g' debian/i3-wm.manpages)
 do
 	[ -f "$file" ] && asciidoc -a linkcss -a stylesdir=https://i3wm.org/css -a scriptsdir=https://i3wm.org/js --backend=xhtml11 -f docs/asciidoc-git.conf "$file"
