i3/i3

commit fff3f79da9a87a1f790c6328f6615422f2b69b47

switch to clang-format-3.8 (#2547)
https://llvm.org/bugs/show_bug.cgi?id=30353 was filed for the unintended
line break between in e.g. “TAILQ_ENTRY(foo)\nbar;”.

Until that’s fixed or a workaround is known, we’ll live with line
breaks. To make it a bit easier for readers to see what’s going on, I
added extra line breaks around each such struct member/variable
definition, so that they at least visually are a single unit.

fixes #2174

.clang-format
@@ -8,3 +8,4 @@ IndentWidth: 4
 PointerBindsToType: false
 ColumnLimit: 0
 SpaceBeforeParens: ControlStatements
+SortIncludes: false

i3bar/include/common.h
@@ -74,10 +74,12 @@ struct status_block {
     char *name;
     char *instance;
 
-    TAILQ_ENTRY(status_block) blocks;
+    TAILQ_ENTRY(status_block)
+    blocks;
 };
 
-TAILQ_HEAD(statusline_head, status_block) statusline_head;
+TAILQ_HEAD(statusline_head, status_block)
+statusline_head;
 
 #include "child.h"
 #include "ipc.h"

i3bar/include/configuration.h
@@ -28,18 +28,23 @@ typedef struct binding_t {
     int input_code;
     char *command;
 
-    TAILQ_ENTRY(binding_t) bindings;
+    TAILQ_ENTRY(binding_t)
+    bindings;
 } binding_t;
 
 typedef struct tray_output_t {
     char *output;
 
-    TAILQ_ENTRY(tray_output_t) tray_outputs;
+    TAILQ_ENTRY(tray_output_t)
+    tray_outputs;
 } tray_output_t;
 
 typedef struct config_t {
     int modifier;
-    TAILQ_HEAD(bindings_head, binding_t) bindings;
+
+    TAILQ_HEAD(bindings_head, binding_t)
+    bindings;
+
     position_t position;
     int verbose;
     struct xcb_color_strings_t colors;
@@ -50,7 +55,10 @@ typedef struct config_t {
     char *command;
     char *fontname;
     i3String *separator_symbol;
-    TAILQ_HEAD(tray_outputs_head, tray_output_t) tray_outputs;
+
+    TAILQ_HEAD(tray_outputs_head, tray_output_t)
+    tray_outputs;
+
     int tray_padding;
     int num_outputs;
     char **outputs;

i3bar/include/outputs.h
@@ -67,5 +67,6 @@ struct i3_output {
     struct ws_head* workspaces;  /* The workspaces on this output */
     struct tc_head* trayclients; /* The tray clients on this output */
 
-    SLIST_ENTRY(i3_output) slist; /* Pointer for the SLIST-Macro */
+    SLIST_ENTRY(i3_output)
+    slist; /* Pointer for the SLIST-Macro */
 };

i3bar/include/trayclients.h
@@ -18,5 +18,6 @@ struct trayclient {
     bool mapped;      /* Whether this window is mapped */
     int xe_version;   /* The XEMBED version supported by the client */
 
-    TAILQ_ENTRY(trayclient) tailq; /* Pointer for the TAILQ-Macro */
+    TAILQ_ENTRY(trayclient)
+    tailq; /* Pointer for the TAILQ-Macro */
 };

i3bar/include/workspaces.h
@@ -40,5 +40,6 @@ struct i3_ws {
     rect rect;                /* The rect of the ws (not used (yet)) */
     struct i3_output *output; /* The current output of the ws */
 
-    TAILQ_ENTRY(i3_ws) tailq; /* Pointer for the TAILQ-Macro */
+    TAILQ_ENTRY(i3_ws)
+    tailq; /* Pointer for the TAILQ-Macro */
 };

include/configuration.h
@@ -68,7 +68,8 @@ struct Variable {
     char *value;
     char *next_match;
 
-    SLIST_ENTRY(Variable) variables;
+    SLIST_ENTRY(Variable)
+    variables;
 };
 
 /**
@@ -82,7 +83,8 @@ struct Mode {
     bool pango_markup;
     struct bindings_head *bindings;
 
-    SLIST_ENTRY(Mode) modes;
+    SLIST_ENTRY(Mode)
+    modes;
 };
 
 /**
@@ -253,7 +255,8 @@ struct Barconfig {
     /* List of outputs on which the tray is allowed to be shown, in order.
      * The special value "none" disables it (per default, it will be shown) and
      * the special value "primary" enabled it on the primary output. */
-    TAILQ_HEAD(tray_outputs_head, tray_output_t) tray_outputs;
+    TAILQ_HEAD(tray_outputs_head, tray_output_t)
+    tray_outputs;
 
     /* Padding around the tray icons. */
     int tray_padding;
@@ -284,7 +287,8 @@ struct Barconfig {
         M_MOD5 = 7
     } modifier;
 
-    TAILQ_HEAD(bar_bindings_head, Barbinding) bar_bindings;
+    TAILQ_HEAD(bar_bindings_head, Barbinding)
+    bar_bindings;
 
     /** Bar position (bottom by default). */
     enum { P_BOTTOM = 0,
@@ -351,7 +355,8 @@ struct Barconfig {
         char *binding_mode_text;
     } colors;
 
-    TAILQ_ENTRY(Barconfig) configs;
+    TAILQ_ENTRY(Barconfig)
+    configs;
 };
 
 /**
@@ -366,13 +371,15 @@ struct Barbinding {
     /** The command which is to be executed for this button. */
     char *command;
 
-    TAILQ_ENTRY(Barbinding) bindings;
+    TAILQ_ENTRY(Barbinding)
+    bindings;
 };
 
 struct tray_output_t {
     char *output;
 
-    TAILQ_ENTRY(tray_output_t) tray_outputs;
+    TAILQ_ENTRY(tray_output_t)
+    tray_outputs;
 };
 
 /**

include/data.h
@@ -199,7 +199,8 @@ struct Workspace_Assignment {
     char *name;
     char *output;
 
-    TAILQ_ENTRY(Workspace_Assignment) ws_assignments;
+    TAILQ_ENTRY(Workspace_Assignment)
+    ws_assignments;
 };
 
 struct Ignore_Event {
@@ -207,7 +208,8 @@ struct Ignore_Event {
     int response_type;
     time_t added;
 
-    SLIST_ENTRY(Ignore_Event) ignore_events;
+    SLIST_ENTRY(Ignore_Event)
+    ignore_events;
 };
 
 /**
@@ -226,7 +228,8 @@ struct Startup_Sequence {
      * completed) */
     time_t delete_at;
 
-    TAILQ_ENTRY(Startup_Sequence) sequences;
+    TAILQ_ENTRY(Startup_Sequence)
+    sequences;
 };
 
 /**
@@ -252,7 +255,9 @@ struct regex {
 struct Binding_Keycode {
     xcb_keycode_t keycode;
     i3_event_state_mask_t modifiers;
-    TAILQ_ENTRY(Binding_Keycode) keycodes;
+
+    TAILQ_ENTRY(Binding_Keycode)
+    keycodes;
 };
 
 /******************************************************************************
@@ -309,12 +314,14 @@ struct Binding {
     /** Only in use if symbol != NULL. Contains keycodes which generate the
      * specified symbol. Useful for unbinding and checking which binding was
      * used when a key press event comes in. */
-    TAILQ_HEAD(keycodes_head, Binding_Keycode) keycodes_head;
+    TAILQ_HEAD(keycodes_head, Binding_Keycode)
+    keycodes_head;
 
     /** Command, like in command mode */
     char *command;
 
-    TAILQ_ENTRY(Binding) bindings;
+    TAILQ_ENTRY(Binding)
+    bindings;
 };
 
 /**
@@ -330,8 +337,12 @@ struct Autostart {
     /** no_startup_id flag for start_application(). Determines whether a
      * startup notification context/ID should be created. */
     bool no_startup_id;
-    TAILQ_ENTRY(Autostart) autostarts;
-    TAILQ_ENTRY(Autostart) autostarts_always;
+
+    TAILQ_ENTRY(Autostart)
+    autostarts;
+
+    TAILQ_ENTRY(Autostart)
+    autostarts_always;
 };
 
 /**
@@ -364,7 +375,8 @@ struct xoutput {
     /** x, y, width, height */
     Rect rect;
 
-    TAILQ_ENTRY(xoutput) outputs;
+    TAILQ_ENTRY(xoutput)
+    outputs;
 };
 
 /**
@@ -493,7 +505,8 @@ struct Match {
            M_ASSIGN_WS,
            M_BELOW } insert_where;
 
-    TAILQ_ENTRY(Match) matches;
+    TAILQ_ENTRY(Match)
+    matches;
 
     /* Whether this match was generated when restarting i3 inplace.
      * Leads to not setting focus when managing a new window, because the old
@@ -537,7 +550,8 @@ struct Assignment {
         char *workspace;
     } dest;
 
-    TAILQ_ENTRY(Assignment) assignments;
+    TAILQ_ENTRY(Assignment)
+    assignments;
 };
 
 /** Fullscreen modes. Used by Con.fullscreen_mode. */
@@ -548,7 +562,8 @@ typedef enum { CF_NONE = 0,
 struct mark_t {
     char *name;
 
-    TAILQ_ENTRY(mark_t) marks;
+    TAILQ_ENTRY(mark_t)
+    marks;
 };
 
 /**
@@ -612,7 +627,8 @@ struct Con {
     char *sticky_group;
 
     /* user-definable marks to jump to this container later */
-    TAILQ_HEAD(marks_head, mark_t) marks_head;
+    TAILQ_HEAD(marks_head, mark_t)
+    marks_head;
     /* cached to decide whether a redraw is needed */
     bool mark_changed;
 
@@ -631,12 +647,17 @@ struct Con {
     struct deco_render_params *deco_render_params;
 
     /* Only workspace-containers can have floating clients */
-    TAILQ_HEAD(floating_head, Con) floating_head;
+    TAILQ_HEAD(floating_head, Con)
+    floating_head;
 
-    TAILQ_HEAD(nodes_head, Con) nodes_head;
-    TAILQ_HEAD(focus_head, Con) focus_head;
+    TAILQ_HEAD(nodes_head, Con)
+    nodes_head;
 
-    TAILQ_HEAD(swallow_head, Match) swallow_head;
+    TAILQ_HEAD(focus_head, Con)
+    focus_head;
+
+    TAILQ_HEAD(swallow_head, Match)
+    swallow_head;
 
     fullscreen_mode_t fullscreen_mode;
 
@@ -674,10 +695,17 @@ struct Con {
         FLOATING_USER_ON = 3
     } floating;
 
-    TAILQ_ENTRY(Con) nodes;
-    TAILQ_ENTRY(Con) focused;
-    TAILQ_ENTRY(Con) all_cons;
-    TAILQ_ENTRY(Con) floating_windows;
+    TAILQ_ENTRY(Con)
+    nodes;
+
+    TAILQ_ENTRY(Con)
+    focused;
+
+    TAILQ_ENTRY(Con)
+    all_cons;
+
+    TAILQ_ENTRY(Con)
+    floating_windows;
 
     /** callbacks */
     void (*on_remove_child)(Con *);

commit e3a1c5b29422303106833f08c6c29c1f20091ab8

clang-format: use TypenameMacros
This commit removes line breaks and extra empty lines introduced in
commit fff3f79da9a87a1f790c6328f6615422f2b69b47.

.clang-format
@@ -10,3 +10,4 @@ ColumnLimit: 0
 SpaceBeforeParens: ControlStatements
 SortIncludes: false
 ForEachMacros: [ TAILQ_FOREACH, TAILQ_FOREACH_REVERSE, SLIST_FOREACH, CIRCLEQ_FOREACH, CIRCLEQ_FOREACH_REVERSE, NODES_FOREACH, NODES_FOREACH_REVERSE, FOREACH_NONINTERNAL]
+TypenameMacros: [ SLIST_HEAD, SLIST_ENTRY, LIST_HEAD, LIST_ENTRY, SIMPLEQ_HEAD, SIMPLEQ_ENTRY, TAILQ_HEAD, TAILQ_ENTRY, CIRCLEQ_HEAD, CIRCLEQ_ENTRY ]

i3bar/include/common.h
@@ -77,12 +77,10 @@ struct status_block {
     char *name;
     char *instance;
 
-    TAILQ_ENTRY(status_block)
-    blocks;
+    TAILQ_ENTRY(status_block) blocks;
 };
 
-extern TAILQ_HEAD(statusline_head, status_block)
-    statusline_head;
+extern TAILQ_HEAD(statusline_head, status_block) statusline_head;
 
 #include "child.h"
 #include "ipc.h"

i3bar/include/configuration.h
@@ -29,23 +29,18 @@ typedef struct binding_t {
     char *command;
     bool release;
 
-    TAILQ_ENTRY(binding_t)
-    bindings;
+    TAILQ_ENTRY(binding_t) bindings;
 } binding_t;
 
 typedef struct tray_output_t {
     char *output;
 
-    TAILQ_ENTRY(tray_output_t)
-    tray_outputs;
+    TAILQ_ENTRY(tray_output_t) tray_outputs;
 } tray_output_t;
 
 typedef struct config_t {
     uint32_t modifier;
-
-    TAILQ_HEAD(bindings_head, binding_t)
-    bindings;
-
+    TAILQ_HEAD(bindings_head, binding_t) bindings;
     position_t position;
     bool verbose;
     bool transparency;
@@ -59,10 +54,7 @@ typedef struct config_t {
     char *command;
     char *fontname;
     i3String *separator_symbol;
-
-    TAILQ_HEAD(tray_outputs_head, tray_output_t)
-    tray_outputs;
-
+    TAILQ_HEAD(tray_outputs_head, tray_output_t) tray_outputs;
     int tray_padding;
     int num_outputs;
     char **outputs;

i3bar/include/outputs.h
@@ -73,6 +73,5 @@ struct i3_output {
     struct ws_head* workspaces;  /* The workspaces on this output */
     struct tc_head* trayclients; /* The tray clients on this output */
 
-    SLIST_ENTRY(i3_output)
-    slist; /* Pointer for the SLIST-Macro */
+    SLIST_ENTRY(i3_output) slist; /* Pointer for the SLIST-Macro */
 };

i3bar/include/trayclients.h
@@ -21,6 +21,5 @@ struct trayclient {
     char *class_class;
     char *class_instance;
 
-    TAILQ_ENTRY(trayclient)
-    tailq; /* Pointer for the TAILQ-Macro */
+    TAILQ_ENTRY(trayclient) tailq; /* Pointer for the TAILQ-Macro */
 };

i3bar/include/workspaces.h
@@ -41,6 +41,5 @@ struct i3_ws {
     rect rect;                /* The rect of the ws (not used (yet)) */
     struct i3_output *output; /* The current output of the ws */
 
-    TAILQ_ENTRY(i3_ws)
-    tailq; /* Pointer for the TAILQ-Macro */
+    TAILQ_ENTRY(i3_ws) tailq; /* Pointer for the TAILQ-Macro */
 };

include/configuration.h
@@ -66,8 +66,7 @@ struct Variable {
     char *value;
     char *next_match;
 
-    SLIST_ENTRY(Variable)
-    variables;
+    SLIST_ENTRY(Variable) variables;
 };
 
 /**
@@ -81,8 +80,7 @@ struct Mode {
     bool pango_markup;
     struct bindings_head *bindings;
 
-    SLIST_ENTRY(Mode)
-    modes;
+    SLIST_ENTRY(Mode) modes;
 };
 
 /**
@@ -272,8 +270,7 @@ struct Barconfig {
     /* List of outputs on which the tray is allowed to be shown, in order.
      * The special value "none" disables it (per default, it will be shown) and
      * the special value "primary" enabled it on the primary output. */
-    TAILQ_HEAD(tray_outputs_head, tray_output_t)
-    tray_outputs;
+    TAILQ_HEAD(tray_outputs_head, tray_output_t) tray_outputs;
 
     /* Padding around the tray icons. */
     int tray_padding;
@@ -295,8 +292,7 @@ struct Barconfig {
     /** Bar modifier (to show bar when in hide mode). */
     uint32_t modifier;
 
-    TAILQ_HEAD(bar_bindings_head, Barbinding)
-    bar_bindings;
+    TAILQ_HEAD(bar_bindings_head, Barbinding) bar_bindings;
 
     /** Bar position (bottom by default). */
     enum { P_BOTTOM = 0,
@@ -370,8 +366,7 @@ struct Barconfig {
         char *binding_mode_text;
     } colors;
 
-    TAILQ_ENTRY(Barconfig)
-    configs;
+    TAILQ_ENTRY(Barconfig) configs;
 };
 
 /**
@@ -389,15 +384,13 @@ struct Barbinding {
     /** If true, the command will be executed after the button is released. */
     bool release;
 
-    TAILQ_ENTRY(Barbinding)
-    bindings;
+    TAILQ_ENTRY(Barbinding) bindings;
 };
 
 struct tray_output_t {
     char *output;
 
-    TAILQ_ENTRY(tray_output_t)
-    tray_outputs;
+    TAILQ_ENTRY(tray_output_t) tray_outputs;
 };
 
 typedef enum {

include/data.h
@@ -206,8 +206,7 @@ struct Workspace_Assignment {
     char *name;
     char *output;
 
-    TAILQ_ENTRY(Workspace_Assignment)
-    ws_assignments;
+    TAILQ_ENTRY(Workspace_Assignment) ws_assignments;
 };
 
 struct Ignore_Event {
@@ -215,8 +214,7 @@ struct Ignore_Event {
     int response_type;
     time_t added;
 
-    SLIST_ENTRY(Ignore_Event)
-    ignore_events;
+    SLIST_ENTRY(Ignore_Event) ignore_events;
 };
 
 /**
@@ -235,8 +233,7 @@ struct Startup_Sequence {
      * completed) */
     time_t delete_at;
 
-    TAILQ_ENTRY(Startup_Sequence)
-    sequences;
+    TAILQ_ENTRY(Startup_Sequence) sequences;
 };
 
 /**
@@ -262,9 +259,7 @@ struct regex {
 struct Binding_Keycode {
     xcb_keycode_t keycode;
     i3_event_state_mask_t modifiers;
-
-    TAILQ_ENTRY(Binding_Keycode)
-    keycodes;
+    TAILQ_ENTRY(Binding_Keycode) keycodes;
 };
 
 /******************************************************************************
@@ -325,14 +320,12 @@ struct Binding {
     /** Only in use if symbol != NULL. Contains keycodes which generate the
      * specified symbol. Useful for unbinding and checking which binding was
      * used when a key press event comes in. */
-    TAILQ_HEAD(keycodes_head, Binding_Keycode)
-    keycodes_head;
+    TAILQ_HEAD(keycodes_head, Binding_Keycode) keycodes_head;
 
     /** Command, like in command mode */
     char *command;
 
-    TAILQ_ENTRY(Binding)
-    bindings;
+    TAILQ_ENTRY(Binding) bindings;
 };
 
 /**
@@ -348,19 +341,13 @@ struct Autostart {
     /** no_startup_id flag for start_application(). Determines whether a
      * startup notification context/ID should be created. */
     bool no_startup_id;
-
-    TAILQ_ENTRY(Autostart)
-    autostarts;
-
-    TAILQ_ENTRY(Autostart)
-    autostarts_always;
+    TAILQ_ENTRY(Autostart) autostarts;
+    TAILQ_ENTRY(Autostart) autostarts_always;
 };
 
 struct output_name {
     char *name;
-
-    SLIST_ENTRY(output_name)
-    names;
+    SLIST_ENTRY(output_name) names;
 };
 
 /**
@@ -387,8 +374,7 @@ struct xoutput {
     /** List of names for the output.
      * An output always has at least one name; the first name is
      * considered the primary one. */
-    SLIST_HEAD(names_head, output_name)
-    names_head;
+    SLIST_HEAD(names_head, output_name) names_head;
 
     /** Pointer to the Con which represents this output */
     Con *con;
@@ -396,8 +382,7 @@ struct xoutput {
     /** x, y, width, height */
     Rect rect;
 
-    TAILQ_ENTRY(xoutput)
-    outputs;
+    TAILQ_ENTRY(xoutput) outputs;
 };
 
 /**
@@ -548,8 +533,7 @@ struct Match {
            M_ASSIGN_WS,
            M_BELOW } insert_where;
 
-    TAILQ_ENTRY(Match)
-    matches;
+    TAILQ_ENTRY(Match) matches;
 
     /* Whether this match was generated when restarting i3 inplace.
      * Leads to not setting focus when managing a new window, because the old
@@ -596,8 +580,7 @@ struct Assignment {
         char *output;
     } dest;
 
-    TAILQ_ENTRY(Assignment)
-    assignments;
+    TAILQ_ENTRY(Assignment) assignments;
 };
 
 /** Fullscreen modes. Used by Con.fullscreen_mode. */
@@ -608,8 +591,7 @@ typedef enum { CF_NONE = 0,
 struct mark_t {
     char *name;
 
-    TAILQ_ENTRY(mark_t)
-    marks;
+    TAILQ_ENTRY(mark_t) marks;
 };
 
 /**
@@ -673,8 +655,7 @@ struct Con {
     char *sticky_group;
 
     /* user-definable marks to jump to this container later */
-    TAILQ_HEAD(marks_head, mark_t)
-    marks_head;
+    TAILQ_HEAD(marks_head, mark_t) marks_head;
     /* cached to decide whether a redraw is needed */
     bool mark_changed;
 
@@ -693,17 +674,12 @@ struct Con {
     struct deco_render_params *deco_render_params;
 
     /* Only workspace-containers can have floating clients */
-    TAILQ_HEAD(floating_head, Con)
-    floating_head;
+    TAILQ_HEAD(floating_head, Con) floating_head;
 
-    TAILQ_HEAD(nodes_head, Con)
-    nodes_head;
+    TAILQ_HEAD(nodes_head, Con) nodes_head;
+    TAILQ_HEAD(focus_head, Con) focus_head;
 
-    TAILQ_HEAD(focus_head, Con)
-    focus_head;
-
-    TAILQ_HEAD(swallow_head, Match)
-    swallow_head;
+    TAILQ_HEAD(swallow_head, Match) swallow_head;
 
     fullscreen_mode_t fullscreen_mode;
 
@@ -741,17 +717,10 @@ struct Con {
         FLOATING_USER_ON = 3
     } floating;
 
-    TAILQ_ENTRY(Con)
-    nodes;
-
-    TAILQ_ENTRY(Con)
-    focused;
-
-    TAILQ_ENTRY(Con)
-    all_cons;
-
-    TAILQ_ENTRY(Con)
-    floating_windows;
+    TAILQ_ENTRY(Con) nodes;
+    TAILQ_ENTRY(Con) focused;
+    TAILQ_ENTRY(Con) all_cons;
+    TAILQ_ENTRY(Con) floating_windows;
 
     /** callbacks */
     void (*on_remove_child)(Con *);

include/i3.h
@@ -57,7 +57,7 @@ extern xcb_key_symbols_t *keysyms;
 extern char **start_argv;
 extern Display *xlibdpy, *xkbdpy;
 extern int xkb_current_group;
-extern TAILQ_HEAD(bindings_head, Binding) * bindings;
+extern TAILQ_HEAD(bindings_head, Binding) *bindings;
 extern const char *current_binding_mode;
 extern TAILQ_HEAD(autostarts_head, Autostart) autostarts;
 extern TAILQ_HEAD(autostarts_always_head, Autostart) autostarts_always;

include/ipc.h
@@ -40,8 +40,7 @@ typedef struct ipc_client {
     uint8_t *buffer;
     size_t buffer_size;
 
-    TAILQ_ENTRY(ipc_client)
-    clients;
+    TAILQ_ENTRY(ipc_client) clients;
 } ipc_client;
 
 /*

libi3/get_colorpixel.c
@@ -15,12 +15,10 @@ struct Colorpixel {
     char hex[8];
     uint32_t pixel;
 
-    SLIST_ENTRY(Colorpixel)
-    colorpixels;
+    SLIST_ENTRY(Colorpixel) colorpixels;
 };
 
-SLIST_HEAD(colorpixel_head, Colorpixel)
-colorpixels;
+SLIST_HEAD(colorpixel_head, Colorpixel) colorpixels;
 
 /*
  * Returns the colorpixel to use for the given hex color (think of HTML).

src/commands.c
@@ -129,9 +129,7 @@ static Con *maybe_auto_back_and_forth_workspace(Con *workspace) {
  */
 typedef struct owindow {
     Con *con;
-
-    TAILQ_ENTRY(owindow)
-    owindows;
+    TAILQ_ENTRY(owindow) owindows;
 } owindow;
 
 typedef TAILQ_HEAD(owindows_head, owindow) owindows_head;

src/con.c
@@ -514,8 +514,7 @@ Con *con_parent_with_orientation(Con *con, orientation_t orientation) {
 struct bfs_entry {
     Con *con;
 
-    TAILQ_ENTRY(bfs_entry)
-    entries;
+    TAILQ_ENTRY(bfs_entry) entries;
 };
 
 /*
@@ -527,9 +526,7 @@ Con *con_get_fullscreen_con(Con *con, fullscreen_mode_t fullscreen_mode) {
 
     /* TODO: is breadth-first-search really appropriate? (check as soon as
      * fullscreen levels and fullscreen for containers is implemented) */
-    TAILQ_HEAD(bfs_head, bfs_entry)
-    bfs_head = TAILQ_HEAD_INITIALIZER(bfs_head);
-
+    TAILQ_HEAD(bfs_head, bfs_entry) bfs_head = TAILQ_HEAD_INITIALIZER(bfs_head);
     struct bfs_entry *entry = smalloc(sizeof(struct bfs_entry));
     entry->con = con;
     TAILQ_INSERT_TAIL(&bfs_head, entry, entries);

src/ipc.c
@@ -25,8 +25,7 @@
 
 char *current_socketpath = NULL;
 
-TAILQ_HEAD(ipc_client_head, ipc_client)
-all_clients = TAILQ_HEAD_INITIALIZER(all_clients);
+TAILQ_HEAD(ipc_client_head, ipc_client) all_clients = TAILQ_HEAD_INITIALIZER(all_clients);
 
 /*
  * Puts the given socket file descriptor into non-blocking mode or dies if

src/load_layout.c
@@ -44,9 +44,7 @@ struct pending_marks {
  * array. */
 struct focus_mapping {
     int old_id;
-
-    TAILQ_ENTRY(focus_mapping)
-    focus_mappings;
+    TAILQ_ENTRY(focus_mapping) focus_mappings;
 };
 
 static TAILQ_HEAD(focus_mappings_head, focus_mapping) focus_mappings =

src/restore_layout.c
@@ -29,8 +29,7 @@ typedef struct placeholder_state {
     /** The drawable surface */
     surface_t surface;
 
-    TAILQ_ENTRY(placeholder_state)
-    state;
+    TAILQ_ENTRY(placeholder_state) state;
 } placeholder_state;
 
 static TAILQ_HEAD(state_head, placeholder_state) state_head =

src/sighandler.c
@@ -16,9 +16,7 @@ typedef struct dialog_t {
     xcb_colormap_t colormap;
     Rect dims;
     surface_t surface;
-
-    TAILQ_ENTRY(dialog_t)
-    dialogs;
+    TAILQ_ENTRY(dialog_t) dialogs;
 } dialog_t;
 
 static TAILQ_HEAD(dialogs_head, dialog_t) dialogs = TAILQ_HEAD_INITIALIZER(dialogs);

src/x.c
@@ -63,26 +63,18 @@ typedef struct con_state {
 
     char *name;
 
-    CIRCLEQ_ENTRY(con_state)
-    state;
-
-    CIRCLEQ_ENTRY(con_state)
-    old_state;
-
-    TAILQ_ENTRY(con_state)
-    initial_mapping_order;
+    CIRCLEQ_ENTRY(con_state) state;
+    CIRCLEQ_ENTRY(con_state) old_state;
+    TAILQ_ENTRY(con_state) initial_mapping_order;
 } con_state;
 
-CIRCLEQ_HEAD(state_head, con_state)
-state_head =
+CIRCLEQ_HEAD(state_head, con_state) state_head =
     CIRCLEQ_HEAD_INITIALIZER(state_head);
 
-CIRCLEQ_HEAD(old_state_head, con_state)
-old_state_head =
+CIRCLEQ_HEAD(old_state_head, con_state) old_state_head =
     CIRCLEQ_HEAD_INITIALIZER(old_state_head);
 
-TAILQ_HEAD(initial_mapping_head, con_state)
-initial_mapping_head =
+TAILQ_HEAD(initial_mapping_head, con_state) initial_mapping_head =
     TAILQ_HEAD_INITIALIZER(initial_mapping_head);
 
 /*