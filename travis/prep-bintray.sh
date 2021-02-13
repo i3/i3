521949b567683b9f81529614f37b0eb835d57fbf
Michael Stapelberg <stapelberg@users.noreply.github.com>
2021-02-13 09:56:15 +0100

    travis: push i3 autobuild packages to balto instead of bintray (#4345)
    related to https://github.com/i3/i3/issues/4340

--- travis/prep-bintray.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-set -x
-
-sed -i "s,%version%,$(git describe --tags),g" travis/bintray-autobuild-*.json
---
+++ travis/push-balto.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+for fn in distbuild/deb/debian-amd64/*.deb distbuild/deb/debian-i386/*.deb
+do
+    echo "pushing $fn to balto"
+    curl \
+	--header "Authorization: Bearer ${BALTO_TOKEN}" \
+	--form "package=@${fn}" \
+	--form distribution=all \
+	https://i3.baltorepo.com/i3/i3-autobuild/upload/
+done
+
+for fn in distbuild/deb/ubuntu-amd64/*.deb distbuild/deb/ubuntu-i386/*.deb
+do
+    echo "pushing $fn to balto"
+    curl \
+	--header "Authorization: Bearer ${BALTO_TOKEN}" \
+	--form "package=@${fn}" \
+	--form distribution=all \
+	https://i3.baltorepo.com/i3/i3-autobuild-ubuntu/upload/
+done
