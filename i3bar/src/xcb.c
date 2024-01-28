80511c7331def362109f2a3c35e8469c45b38d1b
Alessandro Vinciguerra <30745465+Arc676@users.noreply.github.com>
2024-01-28 10:01:06 +0100

    Reduce repetition and fix repeated shrinking
    Co-authored-by: Orestis Floros <orestisflo@gmail.com>

--- i3bar/src/xcb.c
+++ i3bar/src/xcb.c
@@ -252,6 +252,17 @@ static uint32_t predict_statusline_length(void) {
     return width;
 }
 
+static uint32_t switch_block_to_short(struct status_block *block) {
+    /* Skip blocks that have no short form or are already in short form */
+    if (block->short_text == NULL || block->use_short) {
+        return 0;
+    }
+    uint32_t full = block->render_length;
+    block->use_short = true;
+    predict_block_length(block);
+    return full - block->render_length;
+}
+
 static uint32_t adjust_statusline_length(uint32_t max_length) {
     uint32_t width = predict_statusline_length();
 
@@ -261,28 +272,17 @@ static uint32_t adjust_statusline_length(uint32_t max_length) {
         if (width < max_length) {
             break;
         }
-        /* Skip blocks that have no short form or are already in short form */
-        if (block->short_text == NULL || block->use_short) {
-            continue;
-        }
-        uint32_t full = block->render_length;
-        block->use_short = true;
-        predict_block_length(block);
-        uint32_t diff = full - block->render_length;
-        width -= diff;
+        width -= switch_block_to_short(block);
 
-        /* Provide support for representing a single logical block using multiple JSON blocks:
-           if one block is shortened, ensure that all other blocks with the same name are also
-           shortened such that the entire logical block uses the short form text. */
+        /* Provide support for representing a single logical block using multiple
+         * JSON blocks: if one block is shortened, ensure that all other blocks
+         * with the same name are also shortened such that the entire logical block uses
+         * the short form text. */
         if (block->name) {
             struct status_block *other;
             TAILQ_FOREACH (other, &statusline_head, blocks) {
                 if (other->name && !strcmp(other->name, block->name)) {
-                    uint32_t full = other->render_length;
-                    other->use_short = true;
-                    predict_block_length(other);
-                    uint32_t diff = full - other->render_length;
-                    width -= diff;
+                    width -= switch_block_to_short(other);
                 }
             }
         }
