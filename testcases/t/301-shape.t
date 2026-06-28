i3/i3

commit d26ddcbfe5f0ead61cc55c1f596692d9a89e71f9

draw leaf window decorations on ->frame instead of ->parent->frame
related to https://github.com/i3/i3/issues/3724
fixes https://github.com/i3/i3/issues/1966

src/x.c
@@ -355,29 +355,27 @@ void x_window_kill(xcb_window_t window, kill_window_t kill_window) {
     free(event);
 }
 
-static void x_draw_title_border(Con *con, struct deco_render_params *p) {
-    assert(con->parent != NULL);
-
+static void x_draw_title_border(Con *con, struct deco_render_params *p, surface_t *dest_surface) {
     Rect *dr = &(con->deco_rect);
 
     /* Left */
-    draw_util_rectangle(&(con->parent->frame_buffer), p->color->border,
+    draw_util_rectangle(dest_surface, p->color->border,
                         dr->x, dr->y, 1, dr->height);
 
     /* Right */
-    draw_util_rectangle(&(con->parent->frame_buffer), p->color->border,
+    draw_util_rectangle(dest_surface, p->color->border,
                         dr->x + dr->width - 1, dr->y, 1, dr->height);
 
     /* Top */
-    draw_util_rectangle(&(con->parent->frame_buffer), p->color->border,
+    draw_util_rectangle(dest_surface, p->color->border,
                         dr->x, dr->y, dr->width, 1);
 
     /* Bottom */
-    draw_util_rectangle(&(con->parent->frame_buffer), p->color->border,
+    draw_util_rectangle(dest_surface, p->color->border,
                         dr->x, dr->y + dr->height - 1, dr->width, 1);
 }
 
-static void x_draw_decoration_after_title(Con *con, struct deco_render_params *p) {
+static void x_draw_decoration_after_title(Con *con, struct deco_render_params *p, surface_t *dest_surface) {
     assert(con->parent != NULL);
 
     Rect *dr = &(con->deco_rect);
@@ -389,7 +387,7 @@ static void x_draw_decoration_after_title(Con *con, struct deco_render_params *p
         /* We actually only redraw the far right two pixels as that is the
          * distance we keep from the edge (not the entire border width).
          * Redrawing the entire border would cause text to be cut off. */
-        draw_util_rectangle(&(con->parent->frame_buffer), p->color->background,
+        draw_util_rectangle(dest_surface, p->color->background,
                             dr->x + dr->width - 2 * logical_px(1),
                             dr->y,
                             2 * logical_px(1),
@@ -397,7 +395,7 @@ static void x_draw_decoration_after_title(Con *con, struct deco_render_params *p
     }
 
     /* Redraw the border. */
-    x_draw_title_border(con, p);
+    x_draw_title_border(con, p, dest_surface);
 }
 
 /*
@@ -593,16 +591,24 @@ void x_draw_decoration(Con *con) {
         }
     }
 
+    surface_t *dest_surface = &(parent->frame_buffer);
+    if (con_draw_decoration_into_frame(con)) {
+        DLOG("using con->frame_buffer (for con->name=%s) as dest_surface\n", con->name);
+        dest_surface = &(con->frame_buffer);
+    } else {
+        DLOG("sticking to parent->frame_buffer = %p\n", dest_surface);
+    }
+    DLOG("dest_surface %p is %d x %d (id=0x%08x)\n", dest_surface, dest_surface->width, dest_surface->height, dest_surface->id);
+
     /* If the parent hasn't been set up yet, skip the decoration rendering
      * for now. */
-    if (parent->frame_buffer.id == XCB_NONE)
+    if (dest_surface->id == XCB_NONE)
         goto copy_pixmaps;
 
     /* For the first child, we clear the parent pixmap to ensure there's no
      * garbage left on there. This is important to avoid tearing when using
      * transparency. */
     if (con == TAILQ_FIRST(&(con->parent->nodes_head))) {
-        draw_util_clear_surface(&(con->parent->frame_buffer), COLOR_TRANSPARENT);
         FREE(con->parent->deco_render_params);
     }
 
@@ -612,11 +618,13 @@ void x_draw_decoration(Con *con) {
         goto copy_pixmaps;
 
     /* 4: paint the bar */
-    draw_util_rectangle(&(parent->frame_buffer), p->color->background,
+    DLOG("con->deco_rect = (x=%d, y=%d, w=%d, h=%d)\n",
+         con->deco_rect.x, con->deco_rect.y, con->deco_rect.width, con->deco_rect.height);
+    draw_util_rectangle(dest_surface, p->color->background,
                         con->deco_rect.x, con->deco_rect.y, con->deco_rect.width, con->deco_rect.height);
 
     /* 5: draw title border */
-    x_draw_title_border(con, p);
+    x_draw_title_border(con, p, dest_surface);
 
     /* 6: draw the icon and title */
     int text_offset_y = (con->deco_rect.height - config.font.height) / 2;
@@ -651,7 +659,7 @@ void x_draw_decoration(Con *con) {
                                     ? title_padding
                                     : deco_width - mark_width - title_padding;
 
-            draw_util_text(mark, &(parent->frame_buffer),
+            draw_util_text(mark, dest_surface,
                            p->color->text, p->color->background,
                            con->deco_rect.x + mark_offset_x,
                            con->deco_rect.y + text_offset_y, mark_width);
@@ -725,7 +733,7 @@ void x_draw_decoration(Con *con) {
             break;
     }
 
-    draw_util_text(title, &(parent->frame_buffer),
+    draw_util_text(title, dest_surface,
                    p->color->text, p->color->background,
                    con->deco_rect.x + title_offset_x,
                    con->deco_rect.y + text_offset_y,
@@ -733,7 +741,7 @@ void x_draw_decoration(Con *con) {
     if (has_icon) {
         draw_util_image(
             win->icon,
-            &(parent->frame_buffer),
+            dest_surface,
             con->deco_rect.x + icon_offset_x,
             con->deco_rect.y + logical_px(1),
             icon_size,
@@ -744,7 +752,7 @@ void x_draw_decoration(Con *con) {
         I3STRING_FREE(title);
     }
 
-    x_draw_decoration_after_title(con, p);
+    x_draw_decoration_after_title(con, p, dest_surface);
 copy_pixmaps:
     draw_util_copy_surface(&(con->frame_buffer), &(con->frame), 0, 0, 0, 0, con->rect.width, con->rect.height);
 }
@@ -891,7 +899,7 @@ void x_push_node(Con *con) {
         FREE(state->name);
     }
 
-    if (con->window == NULL) {
+    if (con->window == NULL && (con->layout == L_STACKED || con->layout == L_TABBED)) {
         /* Calculate the height of all window decorations which will be drawn on to
          * this frame. */
         uint32_t max_y = 0, max_height = 0;
@@ -905,6 +913,9 @@ void x_push_node(Con *con) {
         rect.height = max_y + max_height;
         if (rect.height == 0)
             con->mapped = false;
+    } else if (con->window == NULL) {
+        /* not a stacked or tabbed split container */
+        con->mapped = false;
     }
 
     bool need_reshape = false;
@@ -949,10 +960,11 @@ void x_push_node(Con *con) {
 
     /* The pixmap of a borderless leaf container will not be used except
      * for the titlebar in a stack or tabs (issue #1013). */
-    bool is_pixmap_needed = (con->border_style != BS_NONE ||
-                             !con_is_leaf(con) ||
-                             con->parent->layout == L_STACKED ||
-                             con->parent->layout == L_TABBED);
+    bool is_pixmap_needed = ((con_is_leaf(con) && con->border_style != BS_NONE) ||
+                             con->layout == L_STACKED ||
+                             con->layout == L_TABBED);
+    DLOG("Con %p (layout %d), is_pixmap_needed = %s, rect.height = %d\n",
+         con, con->layout, is_pixmap_needed ? "yes" : "no", con->rect.height);
 
     /* The root con and output cons will never require a pixmap. In particular for the
      * __i3 output, this will likely not work anyway because it might be ridiculously
@@ -999,6 +1011,7 @@ void x_push_node(Con *con) {
             int width = MAX((int32_t)rect.width, 1);
             int height = MAX((int32_t)rect.height, 1);
 
+            DLOG("creating %d x %d pixmap for con %p (con->frame_buffer.id = (pixmap_t)0x%08x) (con->frame.id (drawable_t)0x%08x)\n", width, height, con, con->frame_buffer.id, con->frame.id);
             xcb_create_pixmap(conn, win_depth, con->frame_buffer.id, con->frame.id, width, height);
             draw_util_surface_init(conn, &(con->frame_buffer), con->frame_buffer.id,
                                    get_visualtype_by_id(get_visualid_by_depth(win_depth)), width, height);

docs/hacking-howto
@@ -564,9 +564,7 @@ split container.
 ==== Default layout
 
 In default layout, containers are placed horizontally or vertically next to
-each other (depending on the +con->orientation+). If a child is a leaf node (as
-opposed to a split container) and has border style "normal", appropriate space
-will be reserved for its window decoration.
+each other (depending on the +con->orientation+).
 
 ==== Stacked layout
 

docs/ipc
@@ -410,6 +410,14 @@ deco_rect (map)::
 	The coordinates of the *window decoration* inside its container. These
 	coordinates are relative to the container and do not include the actual
 	client window.
+actual_deco_rect (map)::
+	See +deco_rect+. i3 v4.22 changed the way title bars are rendered. Before
+	i3 v4.22, the deco_rect was always relative to the parent coordinates.
+	Starting with i3 v4.22, this remains true for tabbed/stacked containers
+	(actual_deco_rect is identical to deco_rect), but for normal-border leaf
+	containers within vertical/horizontal split containers, actual_deco_rect
+	is relative to the container itself. For more background, see
+	https://github.com/i3/i3/issues/1966
 geometry (map)::
 	The original geometry the window specified when i3 mapped it. Used when
 	switching a window to floating mode, for example.

docs/userguide
@@ -1501,23 +1501,6 @@ Tip: You can find an
 https://github.com/Airblader/i3/wiki/Example-Configuration[example
 configuration] that uses modes to illustrate various gap configurations.
 
-[[gaps_artifacts]]
-==== ⚠ Known issue with gaps: graphical artifacts (black rectangles)
-
-The way i3 renders window title bars results in graphical artifacts (black
-rectangles behind windows) when enabling gaps. In some circumstances, running a
-compositor such as `picom` works around the artifacts.
-
-Another workaround is to disable window title bars entirely:
-
-------------------------
-# You can also use any non-zero value if you'd like to have a border
-default_border pixel 0
-------------------------
-
-See https://github.com/i3/i3/issues/3724[Issue #3724] for more details and
-updates on this issue.
-
 == Configuring i3bar
 
 The bar at the bottom of your monitor is drawn by a separate process called

include/con.h
@@ -421,6 +421,14 @@ Con *con_descend_tiling_focused(Con *con);
 Con *con_descend_direction(Con *con, direction_t direction);
 
 /**
+ * Returns whether the window decoration (title bar) should be drawn into the
+ * X11 frame window of this container (default) or into the X11 frame window of
+ * the parent container (for stacked/tabbed containers).
+ *
+ */
+bool con_draw_decoration_into_frame(Con *con);
+
+/**
  * Returns a "relative" Rect which contains the amount of pixels that need to
  * be added to the original Rect to get the final position (obviously the
  * amount of pixels for normal, 1pixel and borderless are different).

src/click.c
@@ -404,13 +404,20 @@ void handle_button_press(xcb_button_press_event_t *event) {
     }
 
     /* Check if the click was on the decoration of a child */
-    Con *child;
-    TAILQ_FOREACH_REVERSE (child, &(con->nodes_head), nodes_head, nodes) {
-        if (!rect_contains(child->deco_rect, event->event_x, event->event_y))
-            continue;
+    if (con->window != NULL) {
+        if (rect_contains(con->deco_rect, event->event_x, event->event_y)) {
+            route_click(con, event, mod_pressed, CLICK_DECORATION);
+            return;
+        }
+    } else {
+        Con *child;
+        TAILQ_FOREACH_REVERSE (child, &(con->nodes_head), nodes_head, nodes) {
+            if (!rect_contains(child->deco_rect, event->event_x, event->event_y))
+                continue;
 
-        route_click(child, event, mod_pressed, CLICK_DECORATION);
-        return;
+            route_click(child, event, mod_pressed, CLICK_DECORATION);
+            return;
+        }
     }
 
     if (event->child != XCB_NONE) {

src/con.c
@@ -1693,6 +1693,20 @@ static bool has_outer_gaps(gaps_t gaps) {
 }
 
 /*
+ * Returns whether the window decoration (title bar) should be drawn into the
+ * X11 frame window of this container (default) or into the X11 frame window of
+ * the parent container (for stacked/tabbed containers).
+ *
+ */
+bool con_draw_decoration_into_frame(Con *con) {
+    return con_is_leaf(con) &&
+           con->border_style == BS_NORMAL &&
+           (con->parent == NULL ||
+            (con->parent->layout != L_TABBED &&
+             con->parent->layout != L_STACKED));
+}
+
+/*
  * Returns a "relative" Rect which contains the amount of pixels that need to
  * be added to the original Rect to get the final position (obviously the
  * amount of pixels for normal, 1pixel and borderless are different).
@@ -1724,7 +1738,16 @@ Rect con_border_style_rect(Con *con) {
     if (border_style == BS_NONE)
         return (Rect){0, 0, 0, 0};
     if (border_style == BS_NORMAL) {
+        const int deco_height = render_deco_height();
         result = (Rect){border_width, 0, -(2 * border_width), -(border_width)};
+        if (con_draw_decoration_into_frame(con)) {
+            result = (Rect){
+                .x = border_width /* left */,
+                .y = deco_height,
+                .width = -(border_width /* left */ + border_width /* right */),
+                .height = -(border_width /* bottom */ + deco_height),
+            };
+        }
     } else {
         result = (Rect){border_width, border_width, -(2 * border_width), -(2 * border_width)};
     }
@@ -1828,23 +1851,17 @@ void con_set_border_style(Con *con, border_style_t border_style, int border_widt
      * pixels. For the parent, we do the same also for the decoration. */
     Con *parent = con->parent;
     Rect bsr = con_border_style_rect(con);
-    int deco_height = (con->border_style == BS_NORMAL ? render_deco_height() : 0);
 
     con->rect = rect_add(con->rect, bsr);
     parent->rect = rect_add(parent->rect, bsr);
-    parent->rect.y += deco_height;
-    parent->rect.height -= deco_height;
 
     /* Change the border style, get new border/decoration values. */
     con->border_style = border_style;
     con->current_border_width = border_width;
     bsr = con_border_style_rect(con);
-    deco_height = (con->border_style == BS_NORMAL ? render_deco_height() : 0);
 
     con->rect = rect_sub(con->rect, bsr);
     parent->rect = rect_sub(parent->rect, bsr);
-    parent->rect.y -= deco_height;
-    parent->rect.height += deco_height;
 }
 
 /*

src/floating.c
@@ -79,16 +79,22 @@ void floating_check_size(Con *floating_con, bool prefer_height) {
     Rect floating_sane_max_dimensions;
     Con *focused_con = con_descend_focused(floating_con);
 
+    DLOG("deco_rect.height = %d\n", focused_con->deco_rect.height);
     Rect border_rect = con_border_style_rect(focused_con);
     /* We have to do the opposite calculations that render_con() do
      * to get the exact size we want. */
     border_rect.width = -border_rect.width;
-    border_rect.width += 2 * focused_con->border_width;
     border_rect.height = -border_rect.height;
+
+    /* undo x11 border */
+    border_rect.width += 2 * focused_con->border_width;
     border_rect.height += 2 * focused_con->border_width;
-    if (con_border_style(focused_con) == BS_NORMAL) {
-        border_rect.height += render_deco_height();
-    }
+
+    DLOG("floating_check_size, want min width %d, min height %d, border extra: w=%d, h=%d\n",
+         floating_sane_min_width,
+         floating_sane_min_height,
+         border_rect.width,
+         border_rect.height);
 
     i3Window *window = focused_con->window;
     if (window != NULL) {
@@ -319,9 +325,6 @@ bool floating_enable(Con *con, bool automatic) {
     x_set_name(nc, name);
     free(name);
 
-    /* find the height for the decorations */
-    int deco_height = render_deco_height();
-
     DLOG("Original rect: (%d, %d) with %d x %d\n", con->rect.x, con->rect.y, con->rect.width, con->rect.height);
     DLOG("Geometry = (%d, %d) with %d x %d\n", con->geometry.x, con->geometry.y, con->geometry.width, con->geometry.height);
     nc->rect = con->geometry;
@@ -352,15 +355,10 @@ bool floating_enable(Con *con, bool automatic) {
     }
 
     /* Add pixels for the decoration. */
-    Rect border_style_rect = con_border_style_rect(con);
+    Rect bsr = con_border_style_rect(con);
 
-    nc->rect.height -= border_style_rect.height;
-    nc->rect.width -= border_style_rect.width;
-
-    /* Add some more pixels for the title bar */
-    if (con_border_style(con) == BS_NORMAL) {
-        nc->rect.height += deco_height;
-    }
+    nc->rect.height -= bsr.height;
+    nc->rect.width -= bsr.width;
 
     /* Honor the X11 border */
     nc->rect.height += con->border_width * 2;
@@ -405,14 +403,6 @@ bool floating_enable(Con *con, bool automatic) {
     }
 
     DLOG("Floating rect: (%d, %d) with %d x %d\n", nc->rect.x, nc->rect.y, nc->rect.width, nc->rect.height);
-
-    /* 5: Subtract the deco_height in order to make the floating window appear
-     * at precisely the position it specified in its original geometry (which
-     * is what applications might remember). */
-    deco_height = (con->border_style == BS_NORMAL ? render_deco_height() : 0);
-    nc->rect.y -= deco_height;
-
-    DLOG("Corrected y = %d (deco_height = %d)\n", nc->rect.y, deco_height);
 
     /* render the cons to get initial window_rect correct */
     render_con(nc);

commit 2689f73368b034a9296c70f76f6e62a353165a87

fix titlebars for shaped windows
Fix the bug introduced in d26ddcbf. After said commit, titlebars belong
to the same frame as the window. Thus, we should include them into the
combined shape.

Extend the related testcase. The fixed case is "normal border" w/
"titlebar center".

release-notes/bugfixes/3-fix-shape-titlebar
@@ -1,0 +1,1 @@
+fix titlebars for shaped windows

src/x.c
@@ -406,7 +406,9 @@ static void x_draw_decoration_after_title(Con *con, struct deco_render_params *p
  * number of rectangles.
  *
  */
-static size_t x_get_border_rectangles(Con *con, xcb_rectangle_t rectangles[4]) {
+static size_t x_get_border_rectangles(Con *con,
+                                      bool include_titlebar,
+                                      xcb_rectangle_t rectangles[4]) {
     size_t count = 0;
     int border_style = con_border_style(con);
 
@@ -439,7 +441,10 @@ static size_t x_get_border_rectangles(Con *con, xcb_rectangle_t rectangles[4]) {
             };
         }
         /* pixel border have an additional line at the top */
-        if (border_style == BS_PIXEL && !(borders_to_hide & ADJ_UPPER_SCREEN_EDGE)) {
+        bool add_pixel =
+            border_style == BS_PIXEL && !(borders_to_hide & ADJ_UPPER_SCREEN_EDGE);
+        bool add_titlebar = include_titlebar && con_draw_decoration_into_frame(con);
+        if (add_pixel || add_titlebar) {
             rectangles[count++] = (xcb_rectangle_t){
                 .x = br.x,
                 .y = 0,
@@ -570,7 +575,7 @@ void x_draw_decoration(Con *con) {
          * children are not freely resizable and we want their background color
          * to "shine through". */
         xcb_rectangle_t rectangles[4];
-        size_t rectangles_count = x_get_border_rectangles(con, rectangles);
+        size_t rectangles_count = x_get_border_rectangles(con, false, rectangles);
         for (size_t i = 0; i < rectangles_count; i++) {
             draw_util_rectangle(&(con->frame_buffer), p->color->child_border,
                                 rectangles[i].x,
@@ -878,7 +883,7 @@ static void x_shape_frame(Con *con, xcb_shape_sk_t shape_kind) {
                       con->window_rect.y + con->border_width,
                       con->window->id);
     xcb_rectangle_t rectangles[4];
-    size_t rectangles_count = x_get_border_rectangles(con, rectangles);
+    size_t rectangles_count = x_get_border_rectangles(con, true, rectangles);
     if (rectangles_count) {
         xcb_shape_rectangles(conn, XCB_SHAPE_SO_UNION, shape_kind,
                              XCB_CLIP_ORDERING_UNSORTED, con->frame.id,

testcases/t/301-shape.t
@@ -62,52 +62,94 @@ END_OF_C_CODE
 
 init_ctx($x->get_xcb_conn());
 
-my ($ws, $win1, $win1_focus, $win2, $win2_focus);
+my $ws; # set by run_test
+my ($bg_win, $bg_win_focus, $shaped_win, $shaped_win_focus); # set by the subtests
 
-################################################################################
-# Case 1: make floating window, then set shape
-################################################################################
+subtest 'normal border: make floating window, then set shape', \&run_test => sub {
+    $bg_win = open_floating_window(rect => [50, 50, 200, 200], background_color => '#ff0000');
+    $bg_win_focus = get_focused($ws);
 
-$ws = fresh_workspace;
+    $shaped_win = open_floating_window(rect => [100, 100, 100, 100], background_color => '#00ff00');
+    cmd '[id=' . $shaped_win->id . '] border normal 10px';
+    $shaped_win_focus = get_focused($ws);
+    set_shape($shaped_win->id);
+};
 
-$win1 = open_floating_window(rect => [0, 0, 100, 100], background_color => '#ff0000');
-$win1_focus = get_focused($ws);
+subtest 'normal border: set shape first, then make window floating', \&run_test, => sub {
+    $bg_win = open_window(rect => [50, 50, 200, 200], background_color => '#ff0000');
+    $bg_win_focus = get_focused($ws);
+    cmd 'floating toggle';
 
-$win2 = open_floating_window(rect => [0, 0, 100, 100], background_color => '#00ff00');
-$win2_focus = get_focused($ws);
-set_shape($win2->id);
+    $shaped_win = open_window(rect => [100, 100, 100, 100], background_color => '#00ff00');
+    cmd '[id=' . $shaped_win->id . '] border normal 10px';
+    $shaped_win_focus = get_focused($ws);
+    set_shape($shaped_win->id);
+    cmd 'floating toggle';
+};
 
-$win1->warp_pointer(75, 25);
-sync_with_i3;
-is(get_focused($ws), $win1_focus, 'focus switched to the underlying window');
+subtest 'pixel border: make floating window, then set shape', \&run_test => sub {
+    $bg_win = open_floating_window(rect => [50, 50, 200, 200], background_color => '#ff0000');
+    $bg_win_focus = get_focused($ws);
 
-$win1->warp_pointer(25, 25);
-sync_with_i3;
-is(get_focused($ws), $win2_focus, 'focus switched to the top window');
+    $shaped_win = open_floating_window(rect => [100, 100, 100, 100], background_color => '#00ff00');
+    cmd '[id=' . $shaped_win->id . '] border pixel 10px';
+    $shaped_win_focus = get_focused($ws);
+    set_shape($shaped_win->id);
+};
 
-kill_all_windows;
+subtest 'pixel border: set shape first, then make window floating', \&run_test, => sub {
+    $bg_win = open_window(rect => [50, 50, 200, 200], background_color => '#ff0000');
+    $bg_win_focus = get_focused($ws);
+    cmd 'floating toggle';
 
-################################################################################
-# Case 2: set shape first, then make window floating
-################################################################################
-
-$ws = fresh_workspace;
-
-$win1 = open_window(rect => [0, 0, 100, 100], background_color => '#ff0000');
-$win1_focus = get_focused($ws);
-cmd 'floating toggle';
-
-$win2 = open_window(rect => [0, 0, 100, 100], background_color => '#00ff00');
-$win2_focus = get_focused($ws);
-set_shape($win2->id);
-cmd 'floating toggle';
-
-$win1->warp_pointer(75, 25);
-sync_with_i3;
-is(get_focused($ws), $win1_focus, 'focus switched to the underlying window');
-
-$win1->warp_pointer(25, 25);
-sync_with_i3;
-is(get_focused($ws), $win2_focus, 'focus switched to the top window');
+    $shaped_win = open_window(rect => [100, 100, 100, 100], background_color => '#00ff00');
+    cmd '[id=' . $shaped_win->id . '] border pixel 10px';
+    $shaped_win_focus = get_focused($ws);
+    set_shape($shaped_win->id);
+    cmd 'floating toggle';
+};
 
 done_testing;
+
+sub run_test {
+    my ($setup_sub) = @_;
+    $ws = fresh_workspace;
+    $setup_sub->();
+
+    # Test the input region by observing the focus_follows_mouse behavior.
+    # The visual conunterpart (clip region) is not tested, but it uses the same
+    # code path.
+
+    # 4    5    6
+    #  ┌───┬───┐
+    #  │ 1 │ 2 │
+    # 7├───┴───┤8
+    #  │   3   │
+    #  └───────┘
+    #      9
+    my @points_table = (
+        { x =>  25, y =>  25, opaque => 1, name => '1: window zone A' },
+        { x =>  75, y =>  25, opaque => 0, name => '2: window zone B' },
+        { x =>  50, y =>  75, opaque => 0, name => '3: window zone C' },
+
+        { x =>  -5, y =>  -5, opaque => 1, name => '4: titlebar left' },
+        { x =>  50, y =>  -5, opaque => 1, name => '5: titlebar center' },
+        { x => 105, y =>  -5, opaque => 1, name => '6: titlebar right' },
+
+        { x =>  50, y => 105, opaque => 1, name => '7: border left' },
+        { x =>  -5, y =>  50, opaque => 1, name => '8: border right' },
+        { x => 105, y =>  50, opaque => 1, name => '9: border bottom' },
+    );
+
+    for my $p (@points_table) {
+        $shaped_win->warp_pointer($p->{x}, $p->{y});
+        sync_with_i3;
+        if ($p->{opaque}) {
+            is(get_focused($ws), $shaped_win_focus, "opaque      - $p->{name}");
+        } else {
+            is(get_focused($ws), $bg_win_focus,     "passthrough - $p->{name}");
+        }
+    }
+
+    kill_all_windows;
+}