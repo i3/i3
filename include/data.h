8c41ad63cf271e2ba2c1e0822fc50f56047dbcdd
Jan Vornberger <jan@271.de>
2026-07-27 19:24:12 +0200

    Ignored events: add changelog and reasoning behind macro

--- include/data.h
+++ include/data.h
@@ -245,6 +245,14 @@ typedef struct ignore_event_sequence_t {
     uint32_t value;
 } ignore_event_sequence_t;
 
+/**
+ * We want to prevent confusion between a uint16_t sequence and a uint32_t full sequence.
+ * This macro will check that the provided argument has the correct sizeof() and will
+ * otherwise throw an error at compile-time ("Array size is negative"). At runtime,
+ * the macro will cause the argument to be wrapped and returned by use of the comma
+ * operator expression.
+ *
+ */
 #define ENSURE_FULL_SEQUENCE(sequence)                                    \
     ((void)sizeof(char[(sizeof(sequence) == sizeof(uint32_t)) ? 1 : -1]), \
      ((ignore_event_sequence_t){.value = sequence}))
